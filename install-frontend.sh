9ca45b71c44891c71af1361e94ac9495bebb0688
Deseteral <deseteral@gmail.com>
2018-04-17 19:59:11 +0200

    Frontend installation fixed

--- install-frontend.sh
+++ install-frontend.sh
@@ -5,4 +5,5 @@ cd ./node_modules/lebkuchen-fm-frontend
 npm i
 npm run build
 cd ../..
-cp -R ./node_modules/lebkuchen-fm-frontend/build/* ./src/public/
+mkdir ./dist/public
+cp -R ./node_modules/lebkuchen-fm-frontend/build/* ./dist/public/
