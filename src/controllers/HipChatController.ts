f905d89a70c1fcde72c693c3caa5e25ae9732e5d
Deseteral <deseteral@gmail.com>
2018-04-18 10:23:57 +0200

    Hipchat controller response

--- src/controllers/HipChatController.ts
+++ src/controllers/HipChatController.ts
@@ -1,28 +1,32 @@
 import { Request, Response } from 'express';
 import CommandParser from '../services/CommandParser';
 import CommandExecuter from '../services/CommandExecuter';
-// https://media.giphy.com/media/11StaZ9Lj74oCY/giphy.gif
+
+const FAIL_GIF_URL = 'https://media.giphy.com/media/11StaZ9Lj74oCY/giphy.gif';
 
 /*
  * POST /commands/hipchat
  */
 function postCommand(req: Request, res: Response) {
-  console.log(req.body);
   const text = req.body.item.message.message;
-
   const command = CommandParser.parse(text);
-  if (!!command) {
-    CommandExecuter.execute(command);
+
+  const responseMessage = !!command
+    ? CommandExecuter.execute(command)
+    : FAIL_GIF_URL;
+
+  if (responseMessage !== '') {
+    const hipchatMessage = {
+      message: responseMessage,
+      color: 'green',
+      notify: false,
+      message_format: 'text',
+    };
+
+    res.send(hipchatMessage);
+  } else {
+    res.status(200).end();
   }
-
-  const hipchatMessage = {
-    message: JSON.stringify(command),
-    color: 'green',
-    notify: false,
-    message_format: 'text',
-  };
-
-  res.send(hipchatMessage);
 }
 
 export default {
--- src/services/CommandExecuter.ts
+++ src/services/CommandExecuter.ts
@@ -4,7 +4,7 @@ import SongRepository from '../repositories/SongRepository';
 import IoConnection from '../clients/IoConnection';
 import { QueueActionType } from '../domain/io-messages/QueueMessage';
 
-function handleAdd(argument: AddArgument) {
+function handleAdd(argument: AddArgument) : string {
   const song: Song = {
     name: argument.name,
     youtubeId: argument.id,
@@ -14,39 +14,42 @@ function handleAdd(argument: AddArgument) {
   };
 
   SongRepository.insert(song);
+
+  return `Dodałem utwór "${song.name}" do biblioteki`;
 }
 
-async function handleQueue(argument: QueueArgument) {
-  try {
-    const song = await SongRepository.getByName(argument.id);
-    IoConnection.broadcast('queue', { action: QueueActionType.Add, song });
-  } catch (err) {
-    console.error(err);
-  }
+function handleQueue(argument: QueueArgument) : string {
+  SongRepository.getByName(argument.id)
+    .then(song =>
+      IoConnection.broadcast('queue', { action: QueueActionType.Add, song })
+    )
+    .catch(err => console.error(err));
+
+  return '';
 }
 
 function handleSkip() {
   IoConnection.broadcast('queue', { action: QueueActionType.Skip, song: null });
+  return '';
 }
 
 function handleSay(argument: SayArgument) {
   IoConnection.broadcast('say', { text: argument.text });
+  return '';
 }
 
-function execute(command: Command) {
+function execute(command: Command) : string {
   switch (command.type) {
     case CommandType.Add:
-      handleAdd(command.arguments as AddArgument);
-      break;
+      return handleAdd(command.arguments as AddArgument);
     case CommandType.Queue:
-      handleQueue(command.arguments as QueueArgument);
-      break;
+      return handleQueue(command.arguments as QueueArgument);
     case CommandType.Skip:
-      handleSkip();
-      break;
+      return handleSkip();
     case CommandType.Say:
-      handleSay(command.arguments as SayArgument);
-      break;
+      return handleSay(command.arguments as SayArgument);
+    default:
+      return '';
   }
 }
 
