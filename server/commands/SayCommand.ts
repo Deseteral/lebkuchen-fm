5581ad29b07f631b9afa8715a4e71853e3b994fe
Deseteral <deseteral@gmail.com>
2018-04-23 23:27:36 +0200

    Say command

---
+++ server/commands/SayCommand.ts
@@ -0,0 +1,20 @@
+import CommandDefinition from '../domain/CommandDefinition';
+import IoConnection from '../clients/IoConnection';
+import SayEventMessage from '../domain/event-messages/SayEventMessage';
+
+function say(invocation: string) : Promise<string> {
+  const eventMessage: SayEventMessage = {
+    text: invocation,
+  };
+
+  IoConnection.broadcast('say', eventMessage);
+  return Promise.resolve('');
+}
+
+const commandDefinition: CommandDefinition = {
+  key: 'say',
+  process: say,
+  helpMessage: 'Prosi spikera o odczytanie wiadomości ;)',
+};
+
+export default commandDefinition;
--- server/commands/registry/CommandInitializer.ts
+++ server/commands/registry/CommandInitializer.ts
@@ -1,8 +1,10 @@
 import CommandRegistry from './CommandRegistry';
 import EchoCommand from '../EchoCommand';
+import SayCommand from '../SayCommand';
 
 function initialize() {
   CommandRegistry.register(EchoCommand);
+  CommandRegistry.register(SayCommand);
 }
 
 export default {
