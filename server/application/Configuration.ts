55bc0a9daf7d870d6077f869ec55d581430ad2c3
Deseteral <deseteral@gmail.com>
2018-04-24 00:17:50 +0200

    Oops

--- server/application/Configuration.ts
+++ server/application/Configuration.ts
@@ -3,7 +3,7 @@ interface Configuration {
 }
 
 const configuration: Configuration = {
-  YOUTUBE_API_KEY: 'AIzaSyAFE3S5UUQR7wq99gBRjBN9_-4meA7vCLU', // process.env['YOUTUBE_KEY'] || null,
+  YOUTUBE_API_KEY: process.env['YOUTUBE_KEY'] || null,
 };
 
 export default configuration;
