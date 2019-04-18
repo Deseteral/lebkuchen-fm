c60a89c4a209481afaa7b5236fda1e5d52d8afde
Deseteral <deseteral@gmail.com>
2019-04-18 21:47:30 +0200

    Spock

---
+++ src/test/groovy/xyz/deseteral/lebkuchenfm/SomeTestSpec.groovy
@@ -0,0 +1,10 @@
+package xyz.deseteral.lebkuchenfm
+
+import spock.lang.Specification
+
+class SomeTestSpec extends Specification {
+    def "one plus one should equal two"() {
+        expect:
+        1 + 1 == 2
+    }
+}
