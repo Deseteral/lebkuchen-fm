8e6c39ff2915e8af3caf5ca5aadc7fb9c1e52da7
Deseteral <deseteral@gmail.com>
2019-04-18 21:41:10 +0200

    Spring init

---
+++ src/main/java/xyz/deseteral/lebkuchenfm/LebkuchenFmApplication.java
@@ -0,0 +1,12 @@
+package xyz.deseteral.lebkuchenfm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LebkuchenFmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LebkuchenFmApplication.class, args);
+    }
+}
