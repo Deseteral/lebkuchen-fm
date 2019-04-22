5be5cc5e964d3113da13ec7446c480dfc6d600f2
Deseteral <deseteral@gmail.com>
2019-04-22 11:15:40 +0200

    Optimize imports

--- src/main/java/xyz/deseteral/lebkuchenfm/api/commands/GenericController.java
+++ src/main/java/xyz/deseteral/lebkuchenfm/api/commands/GenericController.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import xyz.deseteral.lebkuchenfm.domain.dto.GenericCommandRequestDto;
--- src/main/java/xyz/deseteral/lebkuchenfm/services/commands/RootCommandProcessor.java
+++ src/main/java/xyz/deseteral/lebkuchenfm/services/commands/RootCommandProcessor.java
@@ -1,6 +1,5 @@
 package xyz.deseteral.lebkuchenfm.services.commands;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import xyz.deseteral.lebkuchenfm.domain.Command;
 import xyz.deseteral.lebkuchenfm.domain.CommandProcessingResponse;
