0dfdda528c3060f2b04d9b07c69ffcc4a49c53d4
Deseteral <deseteral@gmail.com>
2019-10-19 22:14:03 +0200

    Optimize imports

--- src/main/kotlin/xyz/deseteral/lebkuchenfm/domain/x/SoundAlreadyExistsException.kt
+++ src/main/kotlin/xyz/deseteral/lebkuchenfm/domain/x/SoundAlreadyExistsException.kt
@@ -1,6 +1,4 @@
 package xyz.deseteral.lebkuchenfm.domain.x
 
-import java.lang.RuntimeException
-
 class SoundAlreadyExistsException(soundName: String)
     : RuntimeException("Sound with name $soundName already exists")
