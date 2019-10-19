f92796fb3cf1e40116a4c40efdfd4b5d7a589082
Deseteral <deseteral@gmail.com>
2019-10-19 07:45:31 +0200

    Static wrong args message

--- src/main/kotlin/xyz/deseteral/lebkuchenfm/domain/commands/processors/AddXCommandProcessor.kt
+++ src/main/kotlin/xyz/deseteral/lebkuchenfm/domain/commands/processors/AddXCommandProcessor.kt
@@ -5,6 +5,8 @@ import xyz.deseteral.lebkuchenfm.domain.commands.CommandProcessor
 import xyz.deseteral.lebkuchenfm.domain.commands.model.CommandProcessingResponse
 import xyz.deseteral.lebkuchenfm.domain.x.XSoundService
 
+private val MESSAGE_WRONG_ARGS = "Musisz podać nazwę i URL (`addx sound name|url`)"
+
 @Component
 class AddXCommandProcessor(private val xSoundService: XSoundService) : CommandProcessor {
     override val key: String
@@ -17,10 +19,10 @@ class AddXCommandProcessor(private val xSoundService: XSoundService) : CommandPr
         get() = "Dodaje efekt dźwiękowy (`addx sound name|url`)"
 
     override fun process(args: List<String>): CommandProcessingResponse {
-        if (args.isEmpty()) return CommandProcessingResponse("Musisz podać nazwę i URL (`addx sound name|url`)")
+        if (args.isEmpty()) return CommandProcessingResponse(MESSAGE_WRONG_ARGS)
 
         val addxArgs = args[0].split('|').map { it.trim() }.filter { it.isNotEmpty() }
-        if (addxArgs.size != 2) return CommandProcessingResponse("Musisz podać nazwę i URL (`addx sound name|url`)")
+        if (addxArgs.size != 2) return CommandProcessingResponse(MESSAGE_WRONG_ARGS)
 
         val (soundName, url) = addxArgs
         xSoundService.addNewSound(soundName, url)
