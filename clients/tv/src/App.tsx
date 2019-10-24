99f86cc4ea4e21fc8a622a0914c13c85614f057c
Ryszard Chrystyniak <okazet@gmail.com>
2019-10-24 15:10:47 +0200

    mockup logo & counter

--- clients/tv/src/App.tsx
+++ clients/tv/src/App.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'typeface-pacifico';
 import './App.css';
+
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className="player">
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/CnUT2KIvtSM?autoplay=1" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" title="LebkuchenFM Player"></iframe>
+        </div>
+
+        <div className="player-logo">
+            <span className="player-logo-text">
+                LebkuchenFM
+            </span>
+        </div>
+
+        <div className="counter">
+            <div className="counter-face" />
+            <div className="counter-text">
+                10
+            </div>
+        </div>
+
+        <div className="next">
+            <span className="next-label">Juz za chwile</span>
+            <span className="next-title">Kropson - Super buty</span>
+        </div>
+
     </div>
   );
 }
