938192cff18ed5de43620e43c28904f4bf73b57b
Wojciech Dzikowski <dzikowskw@gmail.com>
2019-10-28 21:36:57 +0100

    1. added proper eslint/editorconfig  support 2. fixed npmrc bug 3. fixed console executor param bug 4. linted exisiting files

--- clients/tv/src/services/CommandEmitter.ts
+++ clients/tv/src/services/CommandEmitter.ts
@@ -1,17 +1,17 @@
-import mitt from 'mitt'
+import mitt from 'mitt';
 import SpeechService from './SpeechService';
 
 const CommandEmitter = (() => {
-    const emitter = mitt();
-    return {
-        emitter,
-        observers: [
-            new SpeechService(emitter),
-        ],
-        emit(command:string, ...options:Array<string>) {
-            this.emitter.emit(command, { options });
-        },
-    };
+  const emitter = mitt();
+  return {
+    emitter,
+    observers: [
+      new SpeechService(emitter),
+    ],
+    emit(command:string, ...options:Array<string>) {
+      this.emitter.emit(command, { options });
+    },
+  };
 })();
 
 export default CommandEmitter;
