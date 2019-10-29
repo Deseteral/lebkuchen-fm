c4de226b45c2757c3b1d485107f9e1c72250ee3f
Wojciech Dzikowski <dzikowskw@gmail.com>
2019-10-29 18:40:38 +0100

    removed service worker dummy

--- clients/tv/src/index.tsx
+++ clients/tv/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import ConsoleExecutor from './executors/ConsoleExecutor';
 
 // since speechSynthesis.speak() without user activation is no longer allowed
@@ -11,8 +10,3 @@ import ConsoleExecutor from './executors/ConsoleExecutor';
 window.executeFmCommand = ConsoleExecutor.executeFmCommand;
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
