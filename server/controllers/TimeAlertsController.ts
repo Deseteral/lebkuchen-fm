9883b492184f88120836f7efe8c567c649ab1adb
Deseteral <deseteral@gmail.com>
2020-03-11 13:32:19 +0100

    Time alerts endpoint fix

--- server/controllers/TimeAlertsController.ts
+++ server/controllers/TimeAlertsController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import Configuration from '../application/Configuration';
 
 function getTimeAlerts(req: Request, res: Response) {
-  res.send(Configuration.TIME_ALERTS);
+  res.json(JSON.parse(Configuration.TIME_ALERTS));
 }
 
 export default {
