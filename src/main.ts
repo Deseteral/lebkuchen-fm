6664d093870c56f19f0302562509d308b93dd256
Deseteral <deseteral@gmail.com>
2020-04-26 00:45:59 +0200

    server

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log('test');
+import express from 'express';
+import signale from 'signale';
+
+signale.config({ displayTimestamp: true });
+
+const app = express();
+const port = 3000;
+
+// TODO: Compression
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => signale.info(`LebkuchenFM service started on port ${port}`));
