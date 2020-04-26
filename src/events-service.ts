f74d19ad10fa40fed0a170966151104ef401d9f9
Deseteral <deseteral@gmail.com>
2020-04-26 14:52:53 +0200

    Player state events

--- src/events-service.ts
+++ src/events-service.ts
@@ -1,8 +1,27 @@
 import WebSocket from 'ws';
 
-interface EventData {
-  id: string;
+const PLAYER_STATE_UPDATE = 'PLAYER_STATE_UPDATE';
+interface PlayerStateUpdateEvent {
+  id: typeof PLAYER_STATE_UPDATE;
+  state: PlayerState;
 }
+
+const PLAYER_STATE_REQUEST = 'PLAYER_STATE_REQUEST';
+interface PlaterStateRequestEvent {
+  id: typeof PLAYER_STATE_REQUEST;
+}
+
+type EventData = (PlayerStateUpdateEvent | PlaterStateRequestEvent);
+
+interface PlayerState {
+  time: string;
+  queue: string[];
+}
+
+const EMPTY_PLAYER_STATE: PlayerState = {
+  time: '00:00',
+  queue: [],
+};
 
 const connectionPool : {[wsid: string]: WebSocket} = {};
 
@@ -16,6 +35,19 @@ function connectionOpened(wsid: string, ws: WebSocket) {
 
 function connectionClosed(wsid: string) {
   delete connectionPool[wsid];
+}
+
+function sendPlayerState(receiverWsid: string) {
+  const connectionPoolKeys = Object.keys(connectionPool);
+  const sourceWsid = connectionPoolKeys.find((wsid) => (wsid !== receiverWsid));
+
+  if (!sourceWsid) {
+    const eventData: PlayerStateUpdateEvent = {
+      id: 'PLAYER_STATE_UPDATE',
+      state: EMPTY_PLAYER_STATE,
+    };
+    connectionPool[receiverWsid].send(eventData);
+  }
 }
 
 export {
