1b236100c75995c145a8afef664a5d4f8edefc11
Deseteral <deseteral@gmail.com>
2020-10-01 23:16:44 +0200

    App startup

--- packages/service/src/main.ts
+++ packages/service/src/main.ts
@@ -2,14 +2,26 @@ import http from 'http';
 import path from 'path';
 import express from 'express';
 import compression from 'compression';
+import bodyParser from 'body-parser';
 import * as Configuration from './application/configuration';
 import * as Logger from './application/logger';
 
-const app = express();
-const server = new http.Server(app);
+const app: express.Express = express();
+const server: http.Server = new http.Server(app);
 
-app.use(compression());
-app.use(express.static(path.join(__dirname, 'public')));
+function configureExpress() {
+  app.use(compression());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(express.static(path.join(__dirname, 'public')));
+}
 
-const port = Configuration.read().PORT;
-server.listen(port, () => Logger.info(`LebkuchenFM service started on port ${port}`));
+function runApplication() {
+  const port = Configuration.read().PORT;
+  server.listen(port, () => Logger.info(`LebkuchenFM service started on port ${port}`));
+}
+
+Promise.resolve()
+  .then(configureExpress)
+  .then(runApplication)
+  .catch((err) => Logger.error(err, 'app-init'));
