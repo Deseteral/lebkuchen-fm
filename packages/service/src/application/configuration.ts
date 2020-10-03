5f6ca98765fc462a3beafbbcf4532d47226e0398
Deseteral <deseteral@gmail.com>
2020-10-03 21:52:39 +0200

    Fix config

--- packages/service/src/application/configuration.ts
+++ packages/service/src/application/configuration.ts
@@ -12,8 +12,8 @@ function read(): Configuration {
   return {
     PORT: (process.env.PORT || '9000'),
     DATABASE_NAME: 'lebkuchen-fm',
-    MONGODB_URI: process.env.MONGO_URI || 'mongodb://localhost:27017',
-    YOUTUBE_API_KEY: process.env.YOUTUBE_KEY || null,
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017',
+    YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY || null,
     SLACK_CHANNEL_ID: process.env.SLACK_CHANNEL_ID || '',
     COMMAND_PROMPT: process.env.COMMAND_PROMPT || '/fm',
   };
