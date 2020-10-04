a712946a935a3f610306f2f5e4e65e1a3aff99b4
Deseteral <deseteral@gmail.com>
2020-10-04 13:05:22 +0200

    Actually register the command

--- packages/service/src/commands/registry/command-initializer.ts
+++ packages/service/src/commands/registry/command-initializer.ts
@@ -1,10 +1,12 @@
 import * as CommandRegistry from './command-registry';
 import EchoCommand from '../processors/echo-command';
 import HelpCommand from '../processors/help-commands';
+import ListXCommand from '../processors/list-x-command';
 
 function initialize(): void {
   CommandRegistry.register(EchoCommand);
   CommandRegistry.register(HelpCommand);
+  CommandRegistry.register(ListXCommand);
 }
 
 export {
