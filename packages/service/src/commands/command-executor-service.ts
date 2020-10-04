7ce750c3a03f4f9e76cc1fe6cf69dc0ea46ee912
Deseteral <deseteral@gmail.com>
2020-10-04 10:42:41 +0200

    Simplify error handling

--- packages/service/src/commands/command-executor-service.ts
+++ packages/service/src/commands/command-executor-service.ts
@@ -3,26 +3,23 @@ import * as TextCommandParser from './text-command-parser';
 import * as CommandRegistry from './registry/command-registry';
 import CommandProcessingResponse, { makeSingleTextMessage } from './command-processing-response';
 
+function commandDoesNotExistResponse(): CommandProcessingResponse {
+  return {
+    messages: makeSingleTextMessage('Komenda nie istnieje'),
+    isVisibleToIssuerOnly: true,
+  };
+}
+
 async function processCommand(command: Command): Promise<CommandProcessingResponse> {
   const commandDefinition = CommandRegistry.getRegistry()[command.key];
-  if (!commandDefinition) {
-    return {
-      messages: makeSingleTextMessage('Komenda nie istnieje'),
-      isVisibleToIssuerOnly: true,
-    };
-  }
+  if (!commandDefinition) return commandDoesNotExistResponse();
 
   return commandDefinition.processor(command);
 }
 
 async function processFromText(textCommand: string): Promise<CommandProcessingResponse> {
   const command = TextCommandParser.parse(textCommand);
-  if (!command) {
-    return {
-      messages: makeSingleTextMessage(`Komenda ${textCommand} nie istnieje`),
-      isVisibleToIssuerOnly: true,
-    };
-  }
+  if (!command) return commandDoesNotExistResponse();
 
   return processCommand(command);
 }
