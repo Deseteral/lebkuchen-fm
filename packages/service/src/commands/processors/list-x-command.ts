470798603525e4b52bdbb63d8d9bd77d6d5c4308
Deseteral <deseteral@gmail.com>
2020-10-04 12:54:40 +0200

    listx command

---
+++ packages/service/src/commands/processors/list-x-command.ts
@@ -0,0 +1,32 @@
+import CommandProcessingResponse, { makeSingleTextMessage, MessageBlock } from '../command-processing-response';
+import CommandDefinition from '../registry/command-definition';
+import * as XSoundsService from '../../x-sounds/x-sounds-service';
+
+async function listXCommandProcessor() : Promise<CommandProcessingResponse> {
+  const sounds = await XSoundsService.getAll();
+
+  if (sounds.length === 0) {
+    return {
+      messages: makeSingleTextMessage('Brak dźwięków w bazie'),
+      isVisibleToIssuerOnly: false,
+    };
+  }
+
+  const soundNames: MessageBlock[] = sounds
+    .sort((a, b) => a.name.localeCompare(b.name))
+    .map((sound) => sound.name)
+    .map((soundName) => ({ type: 'PLAIN_TEXT', text: soundName }));
+
+  return {
+    messages: [{ type: 'HEADER', text: 'X Sounds list' }, ...soundNames],
+    isVisibleToIssuerOnly: false,
+  };
+}
+
+const commandDefinition: CommandDefinition = {
+  key: 'listx',
+  processor: listXCommandProcessor,
+  helpMessage: 'Wypisuje listę czaderskich dźwięków w bazie',
+};
+
+export default commandDefinition;
