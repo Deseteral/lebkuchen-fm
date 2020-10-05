dc5a00d823d7c97f6ff7ed7e613d4672a24ccefe
Deseteral <deseteral@gmail.com>
2020-10-05 23:56:33 +0200

    Logging for player event stream

--- packages/fm-player/src/services/event-stream-client.ts
+++ packages/fm-player/src/services/event-stream-client.ts
@@ -4,17 +4,18 @@ import * as PlayerStateService from './player-state-service';
 
 function connect() {
   const client = io('/');
+  client.on('connect', () => console.log('Connected to event stream WebSocket'));
 
   client.on('events', (eventData: EventData, sendResponse: Function) => {
+    console.log('Received event from event stream', eventData);
+
     switch (eventData.id) {
       case 'PlayerStateUpdateEvent':
         PlayerStateService.setState(eventData.state);
-        console.log('new state', eventData.state);
         break;
 
       case 'PlayerStateRequestEvent': {
         const state = PlayerStateService.getState();
-        console.log(state);
         sendResponse(state);
       } break;
 
