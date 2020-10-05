d50b6f5873f930b27a4e1e10ce848d449634a62f
Deseteral <deseteral@gmail.com>
2020-10-05 02:18:49 +0200

    Queue and YouTube player marriage

--- packages/fm-player/src/services/player-state-service.ts
+++ packages/fm-player/src/services/player-state-service.ts
@@ -1,16 +1,40 @@
-import { makeDefaultPlayerState, PlayerState } from 'lebkuchen-fm-service';
+import { makeDefaultPlayerState, PlayerState, Song } from 'lebkuchen-fm-service';
+import mitt, { Emitter, Handler } from 'mitt';
 
 let playerState: PlayerState = makeDefaultPlayerState();
+const emitter: Emitter = mitt();
 
-function getState() {
+function getState(): PlayerState {
   return playerState;
 }
 
-function setState(nextState: PlayerState) {
+function setState(nextState: PlayerState): void {
   playerState = nextState;
+}
+
+function popFromQueueFront(): (Song | null) {
+  const popped = playerState.queue.shift();
+  return popped || null;
+}
+
+function addToQueue(song: Song) {
+  playerState.queue.push(song);
+  if (playerState.queue.length === 1) {
+    emitter.emit('songAddedToQueueFront');
+  }
+}
+
+type PlayerStateEventType = 'songAddedToQueueFront';
+type PlayerStateEvent = void;
+
+function on(eventType: PlayerStateEventType, callback: Handler<PlayerStateEvent>) {
+  emitter.on<PlayerStateEvent>(eventType, callback);
 }
 
 export {
   getState,
   setState,
+  popFromQueueFront,
+  addToQueue,
+  on,
 };
--- packages/fm-player/src/services/youtube-player-service.ts
+++ packages/fm-player/src/services/youtube-player-service.ts
@@ -1,26 +1,48 @@
-import YouTubePlayer from 'youtube-player';
+import YouTubePlayerFactory from 'youtube-player';
+import { YouTubePlayer } from 'youtube-player/dist/types';
+import YouTubePlayerStates from 'youtube-player/dist/constants/PlayerStates';
+import { Song } from 'lebkuchen-fm-service';
+import * as PlayerStateService from './player-state-service';
+
+function playVideo(player: YouTubePlayer, song: Song) {
+  player.loadVideoById(song.youtubeId);
+  player.playVideo();
+}
+
+function playNextVideo(player: YouTubePlayer) {
+  const song = PlayerStateService.popFromQueueFront();
+  if (song) {
+    playVideo(player, song);
+  } else {
+    player.stopVideo();
+  }
+}
 
 function initialize(domId: string) {
-  const player = YouTubePlayer('yt-player');
+  const player = YouTubePlayerFactory(domId);
 
-  // player.on('stateChange', (event) => {
-  //   if (event.data === YT_ENDED) {
-  //     playNextVideo();
-  //   }
-  // });
+  player.on('stateChange', (event) => {
+    if (event.data === YouTubePlayerStates.ENDED) {
+      playNextVideo(player);
+    }
+  });
 
-  // player.on('error', (event) => {
-  //   console.error('ERROR', event);
-  //   playNextVideo();
-  // });
+  player.on('error', (event) => {
+    console.error(event);
+    playNextVideo(player);
+  });
 
-  // youtubeQueue.setOnAddListener(() => {
-  //   player.getPlayerState().then((playerState) => {
-  //     if ([YT_ENDED, YT_CUED].indexOf(playerState) >= 0) {
-  //       playNextVideo();
-  //     }
-  //   });
-  // })
+  PlayerStateService.on('songAddedToQueueFront', () => {
+    const youTubePlayerState = player.getPlayerState();
+    const playerIsReadyToPlayNextVideo = (
+      (youTubePlayerState === YouTubePlayerStates.ENDED) ||
+      (youTubePlayerState === YouTubePlayerStates.VIDEO_CUED)
+    );
+
+    if (playerIsReadyToPlayNextVideo) {
+      playNextVideo(player);
+    }
+  });
 }
 
 export {
