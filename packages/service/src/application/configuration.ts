87cb8e057ff089343b8d63f0dbe09560aed327bb
Deseteral <deseteral@gmail.com>
2020-10-06 22:33:04 +0200

    YOUTUBE_API_KEY default value

--- packages/service/src/application/configuration.ts
+++ packages/service/src/application/configuration.ts
@@ -2,7 +2,7 @@ interface Configuration {
   PORT: string,
   DATABASE_NAME: string,
   MONGODB_URI: string,
-  YOUTUBE_API_KEY: (string | null),
+  YOUTUBE_API_KEY: string,
   SLACK_CHANNEL_ID: string,
   COMMAND_PROMPT: string,
 }
@@ -14,7 +14,7 @@ function read(): Configuration {
     PORT: (process.env.PORT || '9000'),
     DATABASE_NAME: 'lebkuchen-fm',
     MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017',
-    YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY || null,
+    YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY || '',
     SLACK_CHANNEL_ID: process.env.SLACK_CHANNEL_ID || '',
     COMMAND_PROMPT: process.env.COMMAND_PROMPT || '/fm',
   };
