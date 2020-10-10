9d945a2a430346aef6f92b3acc52ba8f740bb0d1
Deseteral <deseteral@gmail.com>
2020-10-10 18:31:57 +0200

    Style

--- packages/service/src/domain/commands/registry/command-initializer.ts
+++ packages/service/src/domain/commands/registry/command-initializer.ts
@@ -5,13 +5,13 @@ import HelpCommand from '../processors/help-commands';
 import ListXCommand from '../processors/list-x-command';
 import PauseCommand from '../processors/pause-command';
 import QueueCommand from '../processors/queue-command';
+import RandomCommand from '../processors/random-command';
 import ResumeCommand from '../processors/resume-command';
 import SayCommand from '../processors/say-command';
-import XCommand from '../processors/x-command';
 import SearchCommand from '../processors/search-command';
 import SkipCommand from '../processors/skip-command';
 import VolumeCommand from '../processors/volume-command';
-import RandomCommand from '../processors/random-command';
+import XCommand from '../processors/x-command';
 
 function initialize(): void {
   CommandRegistry.register(AddCommand);
@@ -20,13 +20,13 @@ function initialize(): void {
   CommandRegistry.register(ListXCommand);
   CommandRegistry.register(PauseCommand);
   CommandRegistry.register(QueueCommand);
+  CommandRegistry.register(RandomCommand);
   CommandRegistry.register(ResumeCommand);
   CommandRegistry.register(SayCommand);
-  CommandRegistry.register(XCommand);
   CommandRegistry.register(SearchCommand);
   CommandRegistry.register(SkipCommand);
   CommandRegistry.register(VolumeCommand);
-  CommandRegistry.register(RandomCommand);
+  CommandRegistry.register(XCommand);
 }
 
 export {
