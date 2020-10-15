baa07e01760c55b30a5150c420947283716b11a0
Deseteral <deseteral@gmail.com>
2020-10-15 15:36:11 +0200

    Render logs in more visually pleasing way

--- packages/admin-panel/src/components/Logs.tsx
+++ packages/admin-panel/src/components/Logs.tsx
@@ -1,11 +1,13 @@
 import { Log } from 'lebkuchen-fm-service';
 import * as React from 'react';
 import styled from 'styled-components';
+import LogLine from './LogLine';
 import Section from './Section';
 
 const LogsSection = styled(Section)`
   height: 450px;
   background: white;
+  overflow: scroll;
 `;
 
 interface LogsProps {
@@ -14,7 +16,11 @@ interface LogsProps {
 
 const Logs: React.FunctionComponent<LogsProps> = ({ logs }) => (
   <LogsSection>
-    {logs.map((log) => (<code>{log.message}</code>))}
+    <table>
+      <tbody>
+        {logs.map((log) => (<LogLine log={log} key={`${log.datetime}${log.message}`} />))}
+      </tbody>
+    </table>
   </LogsSection>
 );
 
