abbec2db291f4db7edca3c0731a1f3dc1eb86b2f
Deseteral <deseteral@gmail.com>
2020-10-15 15:21:01 +0200

    Export logger and admin event types

--- packages/service/src/lib.ts
+++ packages/service/src/lib.ts
@@ -1,10 +1,14 @@
 import { EventData } from './event-stream/model/events';
 import PlayerState, { makeDefaultPlayerState } from './domain/player-state/player-state';
 import Song from './domain/songs/song';
+import { Log } from './infrastructure/logger';
+import { AdminEventData } from './event-stream/model/admin-events';
 
 export {
   EventData,
   PlayerState,
   makeDefaultPlayerState,
   Song,
+  Log,
+  AdminEventData,
 };
