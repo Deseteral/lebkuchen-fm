48d314d80cb3f81f062f3e2dc29913b1791d2de6
Lukasz Bojarski <lukaszbojarski91@gmail.com>
2020-10-29 21:52:36 +0100

    change button to checkbox

--- packages/fm-player/src/components/VolumeChange.tsx
+++ packages/fm-player/src/components/VolumeChange.tsx
@@ -1,36 +1,19 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import * as React from 'react';
-import styled from 'styled-components';
 import * as EventStreamClient from '../services/event-stream-client';
 
-const Button = styled.button`
-  color: white;
-  background: gray;
-  font-size: 16px;
-  padding: 8px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-`;
-
-function updateIgnoring(onButtonClickEvent: (() => void), ignored: boolean, text: string) {
-  EventStreamClient.setIgnoringVolumeChange(ignored);
-
-  return (
-    <>
-      <Button onClick={onButtonClickEvent} type="button">
-        {text}
-      </Button>
-    </>
-  );
-}
-
 function VolumeChange() {
-  const [ignored, setIgnoring] = React.useState<boolean>(false);
-
   return (
     <div>
-      {ignored && updateIgnoring(() => setIgnoring(false), false, 'ignore volume change')}
-      {!ignored && updateIgnoring(() => setIgnoring(true), true, 'not ignore volume change')}
+      <label>
+        <input
+          type="checkbox"
+          onChange={(event) => {
+            EventStreamClient.setIgnoringVolumeChange(event.target.checked);
+          }}
+        />
+        ignore volume change
+      </label>
     </div>
   );
 }
