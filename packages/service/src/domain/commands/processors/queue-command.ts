a9946b4357c7f18220cd365cacc66b5cb89f7936
Lukasz Bojarski <lukaszbojarski91@gmail.com>
2020-10-30 06:22:38 +0100

    add daily limit of using queue next

--- packages/service/src/domain/commands/processors/queue-command.ts
+++ packages/service/src/domain/commands/processors/queue-command.ts
@@ -5,10 +5,38 @@ import CommandProcessingResponse, { makeSingleTextProcessingResponse } from '../
 import CommandDefinition from '../model/command-definition';
 import { AddSongsToQueueEvent } from '../../../event-stream/model/events';
 
+const DAYILY_LIMIT_QUEUE_NEXT = 3;
+
+let countQueueNext = 0;
+let dayOfLastUsingQueueNext = new Date();
+
+function isToday(date: Date): boolean {
+  const today = new Date();
+  return date.getDate() === today.getDate() &&
+  date.getMonth() === today.getMonth() &&
+  date.getFullYear() === today.getFullYear();
+}
+
+function checkDailyLimitQueueNextExceeded() : boolean {
+  if (isToday(dayOfLastUsingQueueNext) && countQueueNext >= DAYILY_LIMIT_QUEUE_NEXT) {
+    return true;
+  }
+  if (!isToday(dayOfLastUsingQueueNext)) {
+    dayOfLastUsingQueueNext = new Date();
+    countQueueNext = 0;
+  }
+  countQueueNext += 1;
+  return false;
+}
+
 async function queueCommandProcessor(command: Command): Promise<CommandProcessingResponse> {
   const commandArgs = command.getArgsByDelimiter(' ');
   const atTheBeginning = (commandArgs.length > 1 && commandArgs[0] === '-n');
   const songName = atTheBeginning ? commandArgs[1] : commandArgs[0];
+
+  if (atTheBeginning && checkDailyLimitQueueNextExceeded()) {
+    throw Error('Sorry Batory, ale dzienny limit wrzucania na początek został osiągnięty. Poczekaj cierpliwie do jutra');
+  }
 
   const song = await SongsService.instance.getSongByNameWithYouTubeIdFallback(songName);
   const eventData: AddSongsToQueueEvent = { id: 'AddSongsToQueueEvent', songs: [song], atTheBeginning };
