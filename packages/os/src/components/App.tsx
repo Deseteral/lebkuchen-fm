390604d243a37f5f314c7d8adbd0b3e1df7a00df
Deseteral <deseteral@gmail.com>
2021-10-02 17:37:02 +0200

    WIP

---
+++ packages/os/src/components/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import PlayerApp from 'lebkuchen-fm-player/src/components/App';
+
+function App() {
+  return (
+    <PlayerApp />
+    // // <>
+    //   {/* <PlayerApp /> */}
+    //   <div>test</div>
+    // // </>
+  );
+}
+
+export default App;
