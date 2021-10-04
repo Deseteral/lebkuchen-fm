8eb2262f9bda27d660b719595b0739b868a46362
Deseteral <deseteral@gmail.com>
2021-10-04 22:09:32 +0200

    Style

--- packages/service/src/domain/commands/registry/command-initializer.ts
+++ packages/service/src/domain/commands/registry/command-initializer.ts
@@ -1,6 +1,6 @@
-import CommandRegistryService from './command-registry-service';
 import AddCommand from '../processors/add-command';
 import AddXCommand from '../processors/add-x-command';
+import CommandRegistryService from './command-registry-service';
 import HelpCommand from '../processors/help-command';
 import ListXCommand from '../processors/list-x-command';
 import PauseCommand from '../processors/pause-command';
@@ -11,13 +11,13 @@ import SayCommand from '../processors/say-command';
 import SearchCommand from '../processors/search-command';
 import SkipCommand from '../processors/skip-command';
 import SpeedCommand from '../processors/speed-command';
+import TagAddCommand from '../processors/tag-add-command';
+import TagList from '../processors/tag-list-command';
+import TagRemove from '../processors/tag-remove-command';
+import TagSearch from '../processors/tag-search-command';
+import TagShow from '../processors/tag-show-command';
 import VolumeCommand from '../processors/volume-command';
 import XCommand from '../processors/x-command';
-import TagAddCommand from '../processors/tag-add-command';
-import TagRemove from '../processors/tag-remove-command';
-import TagShow from '../processors/tag-show-command';
-import TagList from '../processors/tag-list-command';
-import TagSearch from '../processors/tag-search-command';
 
 function initialize(): void {
   [
@@ -33,13 +33,13 @@ function initialize(): void {
     SearchCommand,
     SkipCommand,
     SpeedCommand,
-    VolumeCommand,
     TagAddCommand,
     TagList,
     TagRemove,
-    XCommand,
-    TagShow,
     TagSearch,
+    TagShow,
+    VolumeCommand,
+    XCommand,
   ].forEach((command) => CommandRegistryService.instance.register(command));
 }
 
