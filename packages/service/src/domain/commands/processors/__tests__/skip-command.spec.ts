381a8875493c037161f77841c00f9020c1149b19
Deseteral <deseteral@gmail.com>
2021-10-09 19:12:59 +0200

    Skip command test

---
+++ packages/service/src/domain/commands/processors/__tests__/skip-command.spec.ts
@@ -0,0 +1,91 @@
+import 'reflect-metadata';
+import PlayerEventStream from '../../../../event-stream/player-event-stream';
+import Command from '../../model/command';
+import SkipCommand from '../skip-command';
+
+jest.mock('../../../../event-stream/player-event-stream');
+jest.mock('../../registry/register-command');
+
+describe('Skip command', () => {
+  const PlayerEventStreamMock = PlayerEventStream as unknown as jest.Mock<PlayerEventStream>;
+  const playerEventStream = new PlayerEventStreamMock();
+  const commandProcessor = new SkipCommand(playerEventStream);
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should skip one song by default', async () => {
+    // given
+    const command = new Command('skip', '');
+
+    // when
+    await commandProcessor.execute(command);
+
+    // then
+    expect(playerEventStream.sendToEveryone).toHaveBeenCalledTimes(1);
+    expect(playerEventStream.sendToEveryone).toHaveBeenCalledWith({
+      id: 'SkipEvent',
+      skipAll: false,
+      amount: 1,
+    });
+  });
+
+  it('should skip all songs', async () => {
+    // given
+    const command = new Command('skip', 'all');
+
+    // when
+    await commandProcessor.execute(command);
+
+    // then
+    expect(playerEventStream.sendToEveryone).toHaveBeenCalledTimes(1);
+    expect(playerEventStream.sendToEveryone).toHaveBeenCalledWith({
+      id: 'SkipEvent',
+      skipAll: true,
+      amount: 1,
+    });
+  });
+
+  it('should skip specified amount of songs', async () => {
+    // given
+    const command = new Command('skip', '123');
+
+    // when
+    await commandProcessor.execute(command);
+
+    // then
+    expect(playerEventStream.sendToEveryone).toHaveBeenCalledTimes(1);
+    expect(playerEventStream.sendToEveryone).toHaveBeenCalledWith({
+      id: 'SkipEvent',
+      skipAll: false,
+      amount: 123,
+    });
+  });
+
+  describe('error handling', () => {
+    it('should throw when specified amount is not a number', async () => {
+      // given
+      const command = new Command('skip', 'not-a-number');
+
+      // then
+      await expect(commandProcessor.execute(command)).rejects.toThrow(Error);
+    });
+
+    it('should throw when specified amount is zero', async () => {
+      // given
+      const command = new Command('skip', '0');
+
+      // then
+      await expect(commandProcessor.execute(command)).rejects.toThrow(Error);
+    });
+
+    it('should throw when specified amount is less than zero', async () => {
+      // given
+      const command = new Command('skip', '-123');
+
+      // then
+      await expect(commandProcessor.execute(command)).rejects.toThrow(Error);
+    });
+  });
+});
