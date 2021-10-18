3ed5ab05cc3ae8f960993bf5c7a58ab3ebd0af00
kstodolak <kapi199726@gmail.com>
2021-10-18 23:56:34 +0200

    removed unused arguments in map function

--- packages/fm-player/src/components/SongsQueue/SongsQueueList.tsx
+++ packages/fm-player/src/components/SongsQueue/SongsQueueList.tsx
@@ -8,7 +8,7 @@ function SongsQueueList({ songsQueue }: SongsQueueListProps) {
   return (
     <ol className="overflow-y-auto px-4 max-h-52 custom-scrollbar list-decimal list-inside">
       {
-        songsQueue.map(({ id, name }, index) => (
+        songsQueue.map(({ id, name }) => (
           <li className="leading-none text-gray-200 py-4" key={id}>
             { name }
           </li>
