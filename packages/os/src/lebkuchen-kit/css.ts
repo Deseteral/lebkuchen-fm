4d591a842ed60176eb8a279df9279d37b8c91322
Deseteral <deseteral@gmail.com>
2021-10-24 13:26:14 +0200

    CSS caching

--- packages/os/src/lebkuchen-kit/css.ts
+++ packages/os/src/lebkuchen-kit/css.ts
@@ -2,6 +2,8 @@ import { customAlphabet } from 'nanoid';
 
 const nanoid = customAlphabet('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', 10);
 
+const cache: Record<string, string> = {};
+
 function createStyle(name: string, content: string) {
   const style = document.createElement('style');
   style.innerHTML = `${name} { ${content} }`;
@@ -9,14 +11,28 @@ function createStyle(name: string, content: string) {
 }
 
 function css(input: TemplateStringsArray): string {
+  const content = input[0];
+
+  if (cache[content]) {
+    return cache[content];
+  }
+
   const className = nanoid();
   createStyle(`.${className}`, input[0]);
-
+  cache[content] = className;
   return className;
 }
 
 String.prototype.onHover = function onHover(input: TemplateStringsArray): string { // eslint-disable-line no-extend-native
-  createStyle(`.${this}:hover`, input[0]);
+  const className = `.${this}:hover`;
+  const content = input[0];
+
+  if (Object.values(cache).includes(className)) {
+    return this.toString();
+  }
+
+  createStyle(className, content);
+  cache[content] = className;
   return this.toString();
 };
 
