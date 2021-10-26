d550bbcf1d64e53dde14d096a641fba363b3d8ec
Wojciech Dzikowski <dzikowskw@gmail.com>
2021-10-26 23:40:51 +0200

    Soundboard MVP

---
+++ packages/fm-player/src/components/SoundBoard.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { queueXSound } from '../services/soundboard-service';
+import SoundButton from './SoundButton';
+
+function SoundBoard() {
+  const [sounds, setSounds] = React.useState([]);
+  React.useEffect(() => {
+    async function fetchSounds() {
+      const response = await fetch('/x-sounds');
+      const data = await response.json();
+      setSounds(data.sounds);
+    }
+    fetchSounds();
+  }, []);
+
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8">
+      {sounds.map(({ name, timesPlayed }) => (
+        <SoundButton
+          key={name}
+          name={name}
+          timesPlayed={timesPlayed}
+          onClick={() => queueXSound(name)}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default SoundBoard;
