2b1e8cb66467f1237044841524defd8ee75daa6f
okazet <okazet@gmail.com>
2021-10-28 23:37:25 +0200

    add tag filtering to soundboard

---
+++ packages/fm-player/src/components/Search/Search.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+type SearchProps = {
+  onPhraseChange: (phrase: string) => void,
+}
+
+function Search({ onPhraseChange }: SearchProps) {
+  return (
+    <div className="relative p-4 min-w-full">
+      <input
+        type="search"
+        className="bg-purple-white shadow rounded border-0 p-3 w-6/12"
+        placeholder="Filter sounds..."
+        // eslint-disable-next-line jsx-a11y/no-autofocus
+        autoFocus
+        onChange={(e) => onPhraseChange(e.target.value)}
+      />
+    </div>
+  );
+}
+
+export default Search;
