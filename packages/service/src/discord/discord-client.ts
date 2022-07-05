c450b1202c7d59800084ebf9e4a21f56f52d4f54
Deseteral <deseteral@gmail.com>
2022-07-05 21:18:49 +0200

    Discord client

---
+++ packages/service/src/discord/discord-client.ts
@@ -0,0 +1,82 @@
+import { Service } from 'typedi';
+import { Client, Intents, Interaction, InteractionReplyOptions } from 'discord.js';
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v9';
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { Configuration } from '@service/infrastructure/configuration';
+import { CommandExecutorService } from '@service/domain/commands/command-executor-service';
+import { CommandProcessingResponse } from '@service/domain/commands/model/command-processing-response';
+import { Logger } from '@service/infrastructure/logger';
+
+@Service()
+class DiscordClient {
+  private static logger: Logger = new Logger('discord-client');
+
+  private client: Client;
+  private rest: REST;
+
+  constructor(private configuration: Configuration, private commandExecutorService: CommandExecutorService) {
+    this.client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+    this.rest = new REST({ version: '9' }).setToken(this.configuration.DISCORD_TOKEN);
+
+    this.client.once('ready', () => this.ready());
+    this.client.on('interactionCreate', async (interaction) => this.interactionCreate(interaction));
+  }
+
+  public async login(): Promise<void> {
+    await this.client.login(this.configuration.DISCORD_TOKEN);
+  }
+
+  public async registerCommands(): Promise<void> {
+    try {
+      DiscordClient.logger.info('Registering slash commands');
+
+      const command = new SlashCommandBuilder()
+        .setName(this.configuration.COMMAND_PROMPT.slice(1))
+        .setDescription('LebkuchenFM')
+        .addStringOption((option) => option.setName('command').setDescription('Command').setRequired(true))
+        .toJSON();
+
+      const route = Routes.applicationGuildCommands(
+        this.configuration.DISCORD_CLIENT_ID,
+        this.configuration.DISCORD_GUILD_ID,
+      );
+
+      await this.rest.put(route, { body: [command] });
+
+      DiscordClient.logger.info('Successfully refreshed slash commands');
+    } catch (error) {
+      DiscordClient.logger.error('There was a problem with refreshing slash commands');
+      DiscordClient.logger.withError(error as Error);
+    }
+  }
+
+  private ready(): void {
+    DiscordClient.logger.info('Discord bot is ready');
+  }
+
+  private async interactionCreate(interaction: Interaction): Promise<void> {
+    if (!interaction.isCommand()) return;
+
+    const messageContent = `${this.configuration.COMMAND_PROMPT} ${interaction.options.getString('command', true)}`;
+    const commandProcessingResponse = await this.commandExecutorService.processFromText(messageContent);
+    const response = this.mapCommandProcessingResponseToDiscordResponse(commandProcessingResponse);
+
+    try {
+      await interaction.reply(response);
+    } catch (error) {
+      DiscordClient.logger.error('There was a problem sending command response');
+      DiscordClient.logger.withError(error as Error);
+    }
+  }
+
+  private mapCommandProcessingResponseToDiscordResponse(commandProcessingResponse: CommandProcessingResponse): InteractionReplyOptions {
+    const content = commandProcessingResponse.messages.map((message: any) => message.text || '').join('\n'); // TODO: Convert with formatting
+    return {
+      content,
+      ephemeral: commandProcessingResponse.isVisibleToIssuerOnly,
+    };
+  }
+}
+
+export { DiscordClient };
--- packages/service/src/infrastructure/configuration.ts
+++ packages/service/src/infrastructure/configuration.ts
@@ -7,6 +7,8 @@ class Configuration {
   public readonly YOUTUBE_API_KEY: string;
   public readonly COMMAND_PROMPT: string;
   public readonly DISCORD_TOKEN: string;
+  public readonly DISCORD_CLIENT_ID: string;
+  public readonly DISCORD_GUILD_ID: string;
   public readonly DROPBOX_TOKEN: string;
   public readonly LOCALE: string;
 
@@ -16,6 +18,8 @@ class Configuration {
     YOUTUBE_API_KEY: string,
     COMMAND_PROMPT: string,
     DISCORD_TOKEN: string,
+    DISCORD_CLIENT_ID: string,
+    DISCORD_GUILD_ID: string,
     DROPBOX_TOKEN: string,
     LOCALE: string,
   ) {
@@ -24,6 +28,8 @@ class Configuration {
     this.YOUTUBE_API_KEY = YOUTUBE_API_KEY;
     this.COMMAND_PROMPT = COMMAND_PROMPT;
     this.DISCORD_TOKEN = DISCORD_TOKEN;
+    this.DISCORD_CLIENT_ID = DISCORD_CLIENT_ID;
+    this.DISCORD_GUILD_ID = DISCORD_GUILD_ID;
     this.DROPBOX_TOKEN = DROPBOX_TOKEN;
     this.LOCALE = LOCALE;
   }
@@ -35,6 +41,8 @@ class Configuration {
       process.env.YOUTUBE_API_KEY || '',
       process.env.COMMAND_PROMPT || '/fm',
       process.env.DISCORD_TOKEN || '',
+      process.env.DISCORD_CLIENT_ID || '',
+      process.env.DISCORD_GUILD_ID || '',
       process.env.DROPBOX_TOKEN || '',
       process.env.LOCALE || 'pl',
     );
