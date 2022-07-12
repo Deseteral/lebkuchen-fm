a8aea1dad7a2241d32d580cd4f03529f71968413
Deseteral <deseteral@gmail.com>
2022-07-12 19:27:13 +0200

    Removed unnecessary function

--- packages/service/src/api/text/model/text-command-response-dto.ts
+++ packages/service/src/api/text/model/text-command-response-dto.ts
@@ -1,11 +1,3 @@
-import { CommandProcessingResponse } from '@service/domain/commands/model/command-processing-response';
-
 export interface TextCommandResponseDto {
   textResponse: string,
-}
-
-export function mapCommandProcessingResponseToTextCommandResponseDto(processingResponse: CommandProcessingResponse): TextCommandResponseDto {
-  return {
-    textResponse: processingResponse.message.markdown,
-  };
 }
