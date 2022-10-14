a7f7ce3568a62b05d2a74abfa6bdc47dc0f742cb
Kajetan Tokarski <Kajetan.tokarski@allegro.pl>
2022-10-14 12:31:20 +0200

    refactor ui of add song

--- packages/client/src/xsound-upload-form/components/XSoundUploadForm.tsx
+++ packages/client/src/xsound-upload-form/components/XSoundUploadForm.tsx
@@ -2,33 +2,54 @@ import * as React from 'react';
 import styled from 'styled-components';
 import { HttpError } from 'lebkuchen-fm-service';
 
+const Container = styled.div`
+  display: grid;
+  place-items: center;
+  min-height: 100vh;
+`;
+
+const Box = styled.div`
+  background-color: white;
+  padding: 1rem 2rem;
+`;
+
 const InputGroup = styled.div`
   display: flex;
   flex-direction: column;
-  border: 1px solid grey;
   padding: 8px;
-  max-width: 250px;
+  max-width: 350px;
 `;
 
 const Header = styled.h2`
-  margin: 0;
   margin: 8px;
+  display: grid;
+  place-items: center;
+  font-weight: bold;
+  font-size: 24px;
 `;
 
 const Input = styled.input`
   margin-bottom: 8px;
+  width: 100%;
+  padding: 1rem;
+  border-width: 1px;
+  border-radius: 0.375rem;
+  border-color: rgba(229,231,235);;
 `;
 
 const SubmitButton = styled.input`
   margin-top: 8px;
+  background-color: rgba(37,99,235);
+  color: white;
+  padding: 0.5rem 1.5rem;
+  border-radius: 0.5rem;
 `;
 
 const MessageContainer = styled.div`
-  background-color: grey;
   margin-top: 16px;
 `;
 
-interface XSoundUploadFormProps {}
+interface XSoundUploadFormProps { }
 
 function XSoundUploadForm(_: XSoundUploadFormProps): JSX.Element {
   const inputFile = React.useRef<HTMLInputElement>(null);
@@ -74,16 +95,28 @@ function XSoundUploadForm(_: XSoundUploadFormProps): JSX.Element {
   };
 
   return (
-    <form onSubmit={submit}>
-      <InputGroup>
-        <Header>Add new sound</Header>
-        <Input type="file" name="soundFile" ref={inputFile} />
-        <Input type="text" placeholder="Sound name" name="soundName" ref={inputSoundName} />
-        <SubmitButton type="submit" value="Submit" disabled={isWaitingForResponse} />
-
-        {message && <MessageContainer>{message}</MessageContainer>}
-      </InputGroup>
-    </form>
+    <Container>
+      <Box>
+        <form onSubmit={submit}>
+          <InputGroup>
+            <Header>Add new sound</Header>
+            <Input
+              type="file"
+              name="soundFile"
+              ref={inputFile}
+            />
+            <Input
+              type="text"
+              placeholder="Sound name"
+              name="soundName"
+              ref={inputSoundName}
+            />
+            <SubmitButton type="submit" value="Submit" disabled={isWaitingForResponse} />
+            {message && <MessageContainer>{message}</MessageContainer>}
+          </InputGroup>
+        </form>
+      </Box>
+    </Container>
   );
 }
 
