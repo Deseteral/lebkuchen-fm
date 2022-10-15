4e4584e39a1f4ed0ad61390e3c5f8d2b2b13d6aa
kstodolak <kapi199726@gmail.com>
2022-10-15 16:59:52 +0200

    Added auto scroll feature to admin logs page

--- packages/client/src/admin/components/Logs.tsx
+++ packages/client/src/admin/components/Logs.tsx
@@ -4,6 +4,10 @@ import { Log } from 'lebkuchen-fm-service';
 import { LogLine } from './LogLine';
 import { Section } from './Section';
 
+const RelativeContainer = styled.div`
+  position: relative;
+`;
+
 const Container = styled.div`
   border: 2px solid;
   border-style: inset;
@@ -14,20 +18,92 @@ const Container = styled.div`
   overflow: scroll;
 `;
 
+const ScrollDownButton = styled.button`
+  position: absolute;
+  bottom: 10px;
+  right: 30px;
+  border: none;
+  border-radius: 0;
+  min-height: 23px;
+  min-width: 75px;
+  padding: 0 12px;
+  background: silver;
+  box-shadow: inset -1px -1px #0a0a0a, inset 1px 1px #fff, inset -2px -2px grey, inset 2px 2px #dfdfdf;
+  font-family: sans-serif;
+  font-size: 13px;
+
+  &:active {
+    box-shadow: inset -1px -1px #fff, inset 1px 1px #0a0a0a, inset -2px -2px #dfdfdf, inset 2px 2px grey;
+    padding: 2px 11px 0 13px;
+  }
+`;
+
 interface LogsProps {
   logs: Log[],
 }
 
 function Logs({ logs }: LogsProps) {
+  const containerElement = React.useRef<HTMLDivElement>(null);
+  const [updateScroll, setUpdateScroll] = React.useState<boolean>(false);
+  const [showScrollDownButton, setShowScrollDownButton] = React.useState<boolean>(true);
+
+  const scrollDown = (): void => {
+    if (containerElement.current) {
+      containerElement.current.scrollTo({ left: 0, top: containerElement.current.scrollHeight, behavior: 'smooth' });
+    }
+  }
+
+  const isScrolledToBottom = (): boolean => {
+    if (!containerElement.current) {
+      return false;
+    }
+    const {
+      scrollHeight,
+      scrollTop,
+      clientHeight
+    } = containerElement.current;
+
+    return scrollHeight - scrollTop - clientHeight < 25;
+  }
+
+  const autoScrollToBottom = (): void => {
+    const scrolledToBottom = isScrolledToBottom();
+
+    if (updateScroll && !scrolledToBottom) {
+      setUpdateScroll(false);
+      setShowScrollDownButton(true);
+      return;
+    }
+
+    if (!updateScroll && scrolledToBottom) {
+      setUpdateScroll(true);
+      setShowScrollDownButton(false);
+    }
+  }
+
+
+  React.useEffect(() => {
+    if (updateScroll) {
+      scrollDown();
+    }
+  }, [logs]);
+
   return (
     <Section header="Logs">
-      <Container>
-        <table>
-          <tbody>
-            {logs.map((log) => (<LogLine log={log} key={`${log.datetime}${log.message}`} />))}
-          </tbody>
-        </table>
-      </Container>
+      <RelativeContainer>
+        <Container ref={containerElement} onScroll={autoScrollToBottom}>
+          <table>
+            <tbody>
+              {logs.map((log) => (<LogLine log={log} key={`${log.datetime}${log.message}`} />))}
+            </tbody>
+          </table>
+          {showScrollDownButton && (
+            <ScrollDownButton onClick={scrollDown}>
+              Scroll Down
+            </ScrollDownButton>
+          )}
+        </Container>
+      </RelativeContainer>
     </Section>
   );
 }
