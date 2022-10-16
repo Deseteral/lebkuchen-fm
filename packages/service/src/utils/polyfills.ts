b61b169f693d8b91340ad9e62248347be1117944
Deseteral <deseteral@gmail.com>
2022-10-16 02:13:07 +0200

    Array.countOccurrences and Map.sort methods

--- packages/service/src/utils/polyfills.ts
+++ packages/service/src/utils/polyfills.ts
@@ -11,11 +11,24 @@ Array.prototype.isEmpty = function isEmpty(): boolean { // eslint-disable-line n
   return this.length === 0;
 };
 
+Array.prototype.countOccurrences = function countOccurrences<T>(): Map<T, number> { // eslint-disable-line no-extend-native
+  return this.reduce((acc, e) => acc.set(e, (acc.get(e) || 0) + 1), new Map());
+};
+
+Map.prototype.sort = function sort<K, V>(compareFn: (a: [K, V], b: [K, V]) => number): Map<K, V> { // eslint-disable-line no-extend-native
+  return new Map([...this.entries()].sort(compareFn));
+};
+
 declare global {
   interface Array<T> {
     last(): T,
     randomShuffle(): T[],
     isEmpty(): boolean,
+    countOccurrences(): Map<T, number>,
+  }
+
+  interface Map<K, V> {
+    sort(compareFn: (a: [K, V], b: [K, V]) => number): Map<K, V>
   }
 }
 
