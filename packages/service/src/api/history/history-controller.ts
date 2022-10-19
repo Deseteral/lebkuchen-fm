e8b95b564ad5bfe3b09e1d0989b75a40fbd8e3dc
Deseteral <deseteral@gmail.com>
2022-10-19 09:01:45 +0200

    Fixed circular dependency

--- packages/service/src/api/history/history-controller.ts
+++ packages/service/src/api/history/history-controller.ts
@@ -3,12 +3,16 @@ import { JsonController, Get, Authorized, QueryParam } from 'routing-controllers
 import { HistoryService } from '@service/domain/history/history-service';
 import { HistoryResponseDto } from '@service/api/history/model/history-response-dto';
 import { HistorySummaryResponseDto } from '@service/api/history/model/history-summary-response-dto';
+import { HistorySummaryService } from '@service/domain/history/history-summary-service';
 
 @Service()
 @JsonController('/api/history')
 @Authorized()
 class HistoryController {
-  constructor(private historyService: HistoryService) { }
+  constructor(
+    private historyService: HistoryService,
+    private historySummaryService: HistorySummaryService,
+  ) { }
 
   @Get('/')
   async getHistoryEntries(): Promise<HistoryResponseDto> {
@@ -21,7 +25,7 @@ class HistoryController {
     @QueryParam('from', { required: true }) from: string,
     @QueryParam('to', { required: true }) to: string,
   ): Promise<HistorySummaryResponseDto> {
-    const summary = await this.historyService.generateSummary(from, to);
+    const summary = await this.historySummaryService.generateSummary(from, to);
     return { summary };
   }
 }
