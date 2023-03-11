b4a37e4118914783e338409de5eff31876fd4488
Maciej Swiechowicz <maciej.swiechowicz@allegro.pl>
2023-03-11 21:33:16 +0100

    Draft soundboard pills animations

--- packages/client/src/sound-board/components/SoundButton.tsx
+++ packages/client/src/sound-board/components/SoundButton.tsx
@@ -1,11 +1,26 @@
 import * as React from 'react';
-import { motion } from 'framer-motion';
+import { useEffect, useRef } from 'react';
+import './SoundButton.css';
 
 interface SoundButtonProps {
   name: string,
   timesPlayed: number,
   onClick: () => void,
 }
+
+type refClassListReducerAction = 'add' | 'remove';
+const refClassListReducer = (
+  ref: React.MutableRefObject<Element | null>,
+) => (action: refClassListReducerAction, className: string) => {
+  if (ref.current) {
+    if (action === 'add') {
+      ref.current.classList.add(className);
+    }
+    if (action === 'remove') {
+      ref.current.classList.remove(className);
+    }
+  }
+};
 
 function getBgColor(timesPlayed: number = 0) {
   if (timesPlayed > 200) return 'bg-blue-900 hover:bg-blue-900 text-white';
@@ -15,21 +30,59 @@ function getBgColor(timesPlayed: number = 0) {
   return 'bg-blue-100 hover:bg-blue-200 text-blue-900';
 }
 
-function SoundButton({ name, timesPlayed, onClick }: SoundButtonProps) {
+function SoundButton({ name, timesPlayed, onClick }: SoundButtonProps): JSX.Element {
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const wasVisible = useRef(false);
+  const refClassListDispatch = refClassListReducer(buttonRef);
+
+  const handleIntersection = (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+    entries.forEach((entry: IntersectionObserverEntry) => {
+      if (entry.isIntersecting) {
+        wasVisible.current = true;
+      } else {
+        entry.target.classList.remove('hoverOut');
+      }
+      // TODO Uncomment this for one way animation
+      // if (wasVisible.current) {
+      //   observer.unobserve(entry.target);
+      // }
+      // @ts-ignore
+      entry.target.style.setProperty('--shown', entry.isIntersecting ? '1' : '0');
+      entry.target.classList.toggle('buttonVisible', entry.isIntersecting);
+    });
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleIntersection);
+
+    if (buttonRef.current) {
+      observer.observe((buttonRef.current));
+    }
+  }, []);
+
   return (
-    <motion.button
-      variants={{
-        hidden: { opacity: 0, x: -200 },
-        show: { opacity: 1, x: 0 },
+    <button
+      ref={buttonRef}
+      className={`${getBgColor(timesPlayed)} text-bl font-bold py-2 px-4 rounded-full m-4 button`}
+      onMouseDown={() => {
+        refClassListDispatch('add', 'mouseDown');
+        refClassListDispatch('remove', 'buttonVisible');
+        refClassListDispatch('remove', 'hoverOut');
       }}
-      whileTap={{ scale: 0.95 }}
-      whileHover={{ scale: 1.1 }}
-      className={`${getBgColor(timesPlayed)} text-bl font-bold py-2 px-4 rounded-full m-4`}
+      onMouseUp={() => {
+        refClassListDispatch('add', 'buttonVisible');
+        refClassListDispatch('remove', 'mouseDown');
+      }}
+      onMouseOut={() => {
+        refClassListDispatch('add', 'hoverOut');
+        refClassListDispatch('remove', 'mouseDown');
+      }}
+      onBlur={() => refClassListDispatch('add', 'hoverOut')}
       type="button"
       onClick={onClick}
     >
       {name}
-    </motion.button>
+    </button>
   );
 }
 
