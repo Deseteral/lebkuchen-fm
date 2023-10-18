9626d029d70da432052d7f5fa88222daa6e253f6
Deseteral <deseteral@gmail.com>
2023-10-18 15:24:18 +0200

    Mute sounds user preference

--- packages/client/src/fm-player/services/user-preferences-service.ts
+++ packages/client/src/fm-player/services/user-preferences-service.ts
@@ -1,9 +1,9 @@
 export function getUserPreference<T>(key: string, defaultValue: T): (T | null) {
   try {
     const data = window.localStorage.getItem(key);
-    return data ? JSON.parse(key) : defaultValue;
-  } catch (e) {
-    console.error(`Cannot get user preference "${key}"`);
+    return data ? JSON.parse(data) : defaultValue;
+  } catch (err) {
+    console.error(`Cannot get user preference "${key}"`, err);
     return defaultValue;
   }
 }
@@ -11,7 +11,7 @@ export function getUserPreference<T>(key: string, defaultValue: T): (T | null) {
 export function setUserPreference<T>(key: string, value: T) {
   try {
     window.localStorage.setItem(key, JSON.stringify(value));
-  } catch (e) {
-    console.error(`Cannot set user preference "${key}"`);
+  } catch (err) {
+    console.error(`Cannot set user preference "${key}"`, err);
   }
 }
