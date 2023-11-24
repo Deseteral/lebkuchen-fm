ca8ddde6bbcf45b10b7e9ed245c8947a29ab5c5d
Wojciech Dzikowski <dzikowskw@gmail.com>
2023-11-24 23:52:28 +0100

    WIP Activity and example messages

--- packages/client/src/fm-player/components/Activity/Activity.tsx
+++ packages/client/src/fm-player/components/Activity/Activity.tsx
@@ -4,7 +4,11 @@ import { Toast } from './Toast';
 function Activity() {
   return (
     <div className="p-4 absolute right-0">
-      <Toast />
+      <Toast text="👋 connected" />
+      <Toast text="😢 disconnected" />
+      <Toast text="🗣️ mówienie" />
+      <Toast text="🔊 jeszcze jak" />
+      <Toast text="🔊 long long long dfasdf afad as" />
     </div>
   );
 }
--- packages/client/src/fm-player/components/Activity/Toast.tsx
+++ packages/client/src/fm-player/components/Activity/Toast.tsx
@@ -1,18 +1,18 @@
 import * as React from 'react';
 
-function Toast() {
+interface ToastProps {
+  text: string
+}
+
+function Toast({ text }: ToastProps) {
   return (
-    <div className="max-w-xs bg-white border border-gray-200 rounded-xl shadow-lg dark:bg-gray-800 dark:border-gray-700" role="alert">
-      <div className="flex p-4">
-        <div className="flex-shrink-0">
-          <svg className="flex-shrink-0 h-4 w-4 text-blue-500 mt-0.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
-          </svg>
+    <div className="max-w-xl rounded-xl shadow-lg bg-gray-700 border-gray-700 text-gray-300 text-sm mb-2" role="alert">
+      <div className="flex p-2">
+        <div className="font-bold text-gray-200">
+          username
         </div>
-        <div className="ms-3">
-          <p className="text-sm text-gray-700 dark:text-gray-400">
-            This is a normal message.
-          </p>
+        <div className="ml-1">
+          {text}
         </div>
       </div>
     </div>
