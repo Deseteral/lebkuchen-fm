7529a56e1d3f6d86735ff80f1d3e5f001da6981e
Deseteral <deseteral@gmail.com>
2023-11-30 00:39:28 +0100

    Year option dropdown

--- packages/client/src/yearly-summary/components/yearly-summary.tsx
+++ packages/client/src/yearly-summary/components/yearly-summary.tsx
@@ -1,11 +1,32 @@
 import * as React from 'react';
+import { YearlyHistorySummary } from '@service/domain/history/history-summary';
 import '../yearly-summary.css';
 
 export function YearlySummary() {
+  const [yearOptions, setYearOptions] = React.useState<YearlyHistorySummary[]>([]);
+  const [selectedYearOption, setSelectedYearOption] = React.useState<string | null>(null);
+
+  React.useEffect(() => {
+    fetch('/api/history/summary/years').then((data) => data.json()).then(({ years }) => {
+      setYearOptions(years);
+      setSelectedYearOption(years[0]);
+    });
+  }, []);
+
+  const onYearSelectionChange = (nextValue: string) => {
+    setSelectedYearOption(nextValue);
+  };
+
   return (
-    <>
+    <main className="yearly-summary-container">
       <h1>Yearly summary</h1>
-      <div>helo</div>
-    </>
+      {(yearOptions.length > 0 && selectedYearOption !== null) && (
+      <select value={selectedYearOption} onChange={(e) => onYearSelectionChange(e.target.value)}>
+        {yearOptions.map((yearOption) => (
+          <option value={yearOption.label} key={yearOption.label}>{yearOption.label}</option>
+        ))}
+      </select>
+      )}
+    </main>
   );
 }
