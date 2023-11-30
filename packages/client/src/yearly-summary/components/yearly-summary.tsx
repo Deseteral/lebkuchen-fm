17502a17bb0a4d985cfa44f0701dbbc888c21cbb
Deseteral <deseteral@gmail.com>
2023-11-30 01:00:03 +0100

    Yearly summary data fetching

--- packages/client/src/yearly-summary/components/yearly-summary.tsx
+++ packages/client/src/yearly-summary/components/yearly-summary.tsx
@@ -1,25 +1,42 @@
 import * as React from 'react';
-import { YearlyHistorySummary } from '@service/domain/history/history-summary';
+import { HistorySummary, YearlyHistorySummary } from '@service/domain/history/history-summary';
 import '../yearly-summary.css';
+import { HistorySummaryResponseDto, HistorySummaryYearsResponseDto } from '@service/api/history/model/history-summary-response-dto';
 
 export function YearlySummary() {
   const [yearOptions, setYearOptions] = React.useState<YearlyHistorySummary[]>([]);
   const [selectedYearOption, setSelectedYearOption] = React.useState<string | null>(null);
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [historySummary, setHistorySummary] = React.useState<HistorySummary | null>(null);
+
+  const fetchHistorySummary = async (url: string) => {
+    setIsLoading(true);
+
+    const response: HistorySummaryResponseDto = await (await fetch(url)).json();
+
+    setIsLoading(false);
+    setHistorySummary(response.summary);
+  };
+
+  const onYearSelectionChange = async (nextValue: string) => {
+    setSelectedYearOption(nextValue);
+    const { url } = yearOptions.find((e) => e.label === nextValue)!;
+    await fetchHistorySummary(url);
+  };
 
   React.useEffect(() => {
-    fetch('/api/history/summary/years').then((data) => data.json()).then(({ years }) => {
-      setYearOptions(years);
-      setSelectedYearOption(years[0]);
-    });
+    (async () => {
+      const response: HistorySummaryYearsResponseDto = await (await fetch('/api/history/summary/years')).json();
+      setYearOptions(response.years);
+      setSelectedYearOption(response.years[0].label);
+      await fetchHistorySummary(response.years[0].url);
+    })();
   }, []);
-
-  const onYearSelectionChange = (nextValue: string) => {
-    setSelectedYearOption(nextValue);
-  };
 
   return (
     <main className="yearly-summary-container">
       <h1>Yearly summary</h1>
+
       {(yearOptions.length > 0 && selectedYearOption !== null) && (
         <select value={selectedYearOption} onChange={(e) => onYearSelectionChange(e.target.value)}>
           {yearOptions.map((yearOption) => (
@@ -27,6 +44,11 @@ export function YearlySummary() {
           ))}
         </select>
       )}
+
+      {isLoading && <div>Loading...</div>}
+
+      {!!historySummary && <div>{JSON.stringify(historySummary)}</div>}
+
     </main>
   );
 }
