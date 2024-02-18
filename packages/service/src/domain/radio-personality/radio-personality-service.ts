9b6d9da9165677dee1da532701cd99ad2f02e3cc
Deseteral <deseteral@gmail.com>
2024-02-18 23:30:32 +0100

    Use prompts from database

--- packages/service/src/domain/radio-personality/radio-personality-service.ts
+++ packages/service/src/domain/radio-personality/radio-personality-service.ts
@@ -3,12 +3,14 @@ import { SayCommand } from '@service/domain/commands/processors/say-command';
 import { LLMGenerator } from '@service/infrastructure/llm-generator';
 import Container, { Service } from 'typedi';
 import { Song } from '@service/domain/songs/song';
+import { RadioPersonalityPromptService } from './radio-personality-prompt-service';
+import { RadioPersonalityPromptType } from './radio-personality-prompt';
 
 @Service()
 class RadioPersonalityService {
   private lastOnNowPlayingChangedTime: number = 0;
 
-  constructor(private llmGenerator: LLMGenerator) { }
+  constructor(private llmGenerator: LLMGenerator, private radioPersonalityPromptService: RadioPersonalityPromptService) { }
 
   public async onNowPlayingChanged(song: Song): Promise<void> {
     const timeNow = Date.now();
@@ -17,8 +19,13 @@ class RadioPersonalityService {
     }
     this.lastOnNowPlayingChangedTime = timeNow;
 
-    const prompt = `Jesteś prezenterem radiowym w radiu Lebkuchen FM. W odpowiedzi oczekuję wiadomości którą jako prezenter radiowy wypowiesz na antenie i żadnych dodatkowych informacji. Twoja odpowiedź nie może zawierać znaków które nie mają znaczenia, takich jak "*". Na końcu mojej wiadomości znajduje się informacja o tym jaki utwór aktualnie leci - możesz użyć tej informacji. Jeżeli tytuł utworu nie jest po polsku możesz go przetłumaczyć na język polski. Oczekuje maksymalnie dwa BARDZO KRÓTKIE zdania, maksymalny poziom żartu, możesz obrażać słuchaczy i przeklinać. Właśnie zaczął lecieć utwór "${song.name}".`;
-    const radioPersonalityResponse = await this.llmGenerator.generateTextForPrompt(prompt);
+    const prompt = await this.radioPersonalityPromptService.getPromptForType(RadioPersonalityPromptType.NewSongStartedPlaying);
+    if (!prompt) {
+      return;
+    }
+
+    const promptText = prompt.text.replaceAll('{{currentSongName}}', song.name);
+    const radioPersonalityResponse = await this.llmGenerator.generateTextForPrompt(promptText);
     if (radioPersonalityResponse) {
       const sayCommand = new Command('say', radioPersonalityResponse);
       await Container.get(SayCommand).execute(sayCommand);
@@ -26,8 +33,13 @@ class RadioPersonalityService {
   }
 
   public async onListenerCall(listenerMessage: string): Promise<string | null> {
-    const prompt = `Jesteś prezenterem radiowym w radiu Lebkuchen FM. Właśnie zadzwonił do Ciebie słuchać audycji którą prowadzisz z wiadomością: "${listenerMessage}". Odpowiedz wiadomością która jest odpowiedzią na ten telefon. W odpowiedzi zacytuj oryginalną wiadomość słuchacza. Twoja odpowiedź nie może zawierać znaków które nie mają znaczenia, takich jak "*". Oczekuje dwa zdania, maksymalny poziom żartu, możesz przeklinać. Masz być maksymalnie pomocnym miłym gościem, który zawsze chce pomóc, ale gada głupoty. Musisz być nieświadomy swojej niezadrności i zawsze wpleść coś śmiesznego w swoją wypowiedź. Pamiętaj żeby nawiązać do tego że dzwoni słuchasz audycji radiowej którą prowadzisz.`;
-    const radioPersonalityResponse = await this.llmGenerator.generateTextForPrompt(prompt);
+    const prompt = await this.radioPersonalityPromptService.getPromptForType(RadioPersonalityPromptType.ListenerCalling);
+    if (!prompt) {
+      return null;
+    }
+
+    const promptText = prompt.text.replaceAll('{{listenerMessage}}', listenerMessage);
+    const radioPersonalityResponse = await this.llmGenerator.generateTextForPrompt(promptText);
 
     if (radioPersonalityResponse) {
       const sayCommand = new Command('say', radioPersonalityResponse);
