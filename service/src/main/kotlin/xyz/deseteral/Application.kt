bc9d7b588962fccae4d5a7b1a883d013048db9a5
Deseteral <deseteral@gmail.com>
2024-10-16 10:35:51 +0200

    Initial commit with Ktor backend

---
+++ service/src/main/kotlin/xyz/deseteral/Application.kt
@@ -0,0 +1,14 @@
+package xyz.deseteral
+
+import io.ktor.server.application.Application
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import xyz.deseteral.plugins.configureRouting
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module).start(wait = true)
+}
+
+fun Application.module() {
+    configureRouting()
+}
---
+++ service/src/main/kotlin/xyz/deseteral/plugins/Routing.kt
@@ -0,0 +1,13 @@
+package xyz.deseteral.plugins
+
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.http.content.staticResources
+import io.ktor.server.resources.Resources
+import io.ktor.server.routing.routing
+
+fun Application.configureRouting() {
+    install(Resources)
+
+    routing { staticResources("/", "static") }
+}
