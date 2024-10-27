b488c857ee18f5a97999f776cf0c0089fe2facc4
Deseteral <deseteral@gmail.com>
2024-10-27 08:57:02 +0100

    Added missing field on CommandProcessor

--- service/src/main/kotlin/xyz/lebkuchenfm/domain/commands/CommandProcessor.kt
+++ service/src/main/kotlin/xyz/lebkuchenfm/domain/commands/CommandProcessor.kt
@@ -6,10 +6,27 @@ import xyz.lebkuchenfm.domain.commands.model.CommandProcessingResult
 abstract class CommandProcessor(
     val key: String,
     val shortKey: String?,
-    // TODO: Get the rest of the parameters setup.
-// abstract get helpMessage(): string;
-// abstract get exampleUsages(): string[];
-// abstract get parameters(): CommandParameters;
+    val helpMessage: String,
+    val exampleUsages: List<String>,
+    val parameters: CommandParameters,
 ) {
     abstract fun execute(command: Command): CommandProcessingResult
+}
+
+class CommandParameters(
+    val parameters: List<CommandParameter>,
+    val delimiter: String? = null,
+) {
+    abstract class CommandParameter(
+        val required: Boolean,
+        val names: List<String>,
+    )
+
+    class RequiredCommandParameter(name: String) : CommandParameter(required = true, names = listOf(name))
+
+    class OptionalCommandParameter(name: String) : CommandParameter(required = false, names = listOf(name))
+
+    class RequiredMultiValue(vararg names: String) : CommandParameter(required = true, names.toList())
+
+    class OptionalMultiValue(vararg names: String) : CommandParameter(required = false, names.toList())
 }
--- service/src/main/kotlin/xyz/lebkuchenfm/domain/commands/processors/XCommandProcessor.kt
+++ service/src/main/kotlin/xyz/lebkuchenfm/domain/commands/processors/XCommandProcessor.kt
@@ -1,10 +1,23 @@
 package xyz.lebkuchenfm.domain.commands.processors
 
+import xyz.lebkuchenfm.domain.commands.CommandParameters
 import xyz.lebkuchenfm.domain.commands.CommandProcessor
 import xyz.lebkuchenfm.domain.commands.model.Command
 import xyz.lebkuchenfm.domain.commands.model.CommandProcessingResult
 
-class XCommandProcessor : CommandProcessor(key = "x", shortKey = null) {
+class XCommandProcessor : CommandProcessor(
+    key = "x",
+    shortKey = null,
+    helpMessage = "Puszcza szalony dźwięk!",
+    exampleUsages = listOf("airhorn"),
+    parameters =
+        CommandParameters(
+            parameters =
+                listOf(
+                    CommandParameters.RequiredCommandParameter("sound-name"),
+                ),
+        ),
+) {
     override fun execute(command: Command): CommandProcessingResult {
         TODO("Not yet implemented")
     }
