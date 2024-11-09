03c5fe61620d6845740177298cc7476008159bf7
Deseteral <deseteral@gmail.com>
2024-11-09 21:23:27 +0100

    Todo comment

--- service/src/main/kotlin/xyz/lebkuchenfm/domain/xsounds/XSound.kt
+++ service/src/main/kotlin/xyz/lebkuchenfm/domain/xsounds/XSound.kt
@@ -2,7 +2,7 @@ package xyz.lebkuchenfm.domain.xsounds
 
 data class XSound(
     val name: String,
-    val url: String,
+    val url: String, // TODO: This should be a URL type.
     val addedBy: String?,
     val timesPlayed: Int,
     val tags: List<String>,
