737b0ce88c684c4f0e9b6f459075b11f325ef5a9
Deseteral <deseteral@gmail.com>
2024-11-11 14:42:57 +0100

    Correctly parse incoming WS events

--- service/src/main/kotlin/xyz/lebkuchenfm/api/eventstream/WebSocketEventStream.kt
+++ service/src/main/kotlin/xyz/lebkuchenfm/api/eventstream/WebSocketEventStream.kt
@@ -1,34 +1,26 @@
 package xyz.lebkuchenfm.api.eventstream
 
 import io.github.oshai.kotlinlogging.KotlinLogging
-import io.ktor.websocket.DefaultWebSocketSession
-import io.ktor.websocket.Frame
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.ClassDiscriminatorMode
-import kotlinx.serialization.json.Json
+import io.ktor.serialization.serialize
+import io.ktor.server.websocket.WebSocketServerSession
+import io.ktor.server.websocket.converter
 import xyz.lebkuchenfm.domain.eventstream.Event
 import xyz.lebkuchenfm.domain.eventstream.EventStream
 import java.util.Collections
 
-class Connection(val session: DefaultWebSocketSession)
+class Connection(val session: WebSocketServerSession)
 
 private val logger = KotlinLogging.logger {}
 
 class WebSocketEventStream : EventStream {
     private val connections = Collections.synchronizedSet<Connection>(LinkedHashSet())
 
-    @OptIn(ExperimentalSerializationApi::class)
-    private val json = Json {
-        encodeDefaults = true
-        classDiscriminatorMode = ClassDiscriminatorMode.NONE
-    }
-
     override suspend fun sendToEveryone(event: Event) {
-        connections.forEach {
+        connections.forEach { connection ->
             val dto = event.mapToDto()
-            val text = json.encodeToString(dto)
-            it.session.send(Frame.Text(text))
+            val converter = checkNotNull(connection.session.converter)
+            val frame = converter.serialize(dto)
+            connection.session.send(frame)
         }
     }
 
