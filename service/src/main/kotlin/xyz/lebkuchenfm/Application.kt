f7be2805a104bb1b9627dab531f793a58703cd63
Michal Gorzalczany <michal@gorzalczany.pl>
2024-11-14 00:36:07 +0100

    move index creating to blocking scope

--- service/src/main/kotlin/xyz/lebkuchenfm/Application.kt
+++ service/src/main/kotlin/xyz/lebkuchenfm/Application.kt
@@ -21,6 +21,7 @@ import io.ktor.server.sessions.Sessions
 import io.ktor.server.sessions.cookie
 import io.ktor.server.websocket.WebSockets
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.Json
 import xyz.lebkuchenfm.api.auth.authRouting
 import xyz.lebkuchenfm.api.commands.commandsRouting
@@ -63,7 +64,7 @@ fun Application.module() {
     val xSoundsFileRepository = XSoundsDropboxFileRepository(dropboxClient, environment.config)
     val xSoundsRepository = XSoundsMongoRepository(database)
     val xSoundsService = XSoundsService(xSoundsRepository, xSoundsFileRepository)
-    launch {
+    runBlocking {
         xSoundsRepository.createUniqueIndex()
     }
 
