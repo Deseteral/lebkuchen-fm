155ad8fff827c8891febc0605a92e411a4ef915f
Deseteral <deseteral@gmail.com>
2024-11-17 01:00:43 +0100

    Connect to WS from OS level instead of YT player

--- packages/client-beta/src/apps/Player/components/YouTubePlayer/YouTubePlayer.tsx
+++ packages/client-beta/src/apps/Player/components/YouTubePlayer/YouTubePlayer.tsx
@@ -1,17 +1,14 @@
 import { onMount, onCleanup } from 'solid-js';
 import { YoutubePlayerService } from '../../services/youtube-player-service';
-import { SocketConnectionClient } from '../../../../services/socket-connection-client';
 
 const YOUTUBE_PLAYER_DOM_ID = 'youtube-player';
 
 function YouTubePlayer() {
   onMount(() => {
-    SocketConnectionClient.initializeConnection();
     YoutubePlayerService.initialize(YOUTUBE_PLAYER_DOM_ID);
   });
 
   onCleanup(() => {
-    SocketConnectionClient.disconnect();
     YoutubePlayerService.cleanup();
   });
 
