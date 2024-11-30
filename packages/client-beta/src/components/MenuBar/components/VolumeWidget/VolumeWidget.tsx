59c4b713a8c89ad9be8d1885b71bfa1f331badad
Kacper Stodolak <kapi199726@gmail.com>
2024-11-30 17:06:14 +0100

    VolumeWidget is now only subscribing localPlayerStateUpdate events

--- packages/client-beta/src/components/MenuBar/components/VolumeWidget/VolumeWidget.tsx
+++ packages/client-beta/src/components/MenuBar/components/VolumeWidget/VolumeWidget.tsx
@@ -2,14 +2,35 @@ import volumeLowIcon from '../../../../icons/volume-low-solid.svg';
 import volumeHighIcon from '../../../../icons/volume-high-solid.svg';
 import volumeMuteIcon from '../../../../icons/volume-xmark-solid.svg';
 import styles from './VolumeWidget.module.css';
-import { createEffect, createSignal } from 'solid-js';
-import { PlayerStateService } from '../../../../apps/Player/services/player-state-service';
+import { createEffect, createSignal, onCleanup, onMount } from 'solid-js';
+import { EventStreamClient } from '../../../../services/event-stream-client';
+import { LocalEventTypes, LocalPlayerStateUpdateEvent } from '../../../../types/local-events';
 
 function VolumeWidget() {
   // TODO: Get volume from preferences stored in localStorage
-  const currentVolume = PlayerStateService.get().volume;
-  const [volume] = createSignal(currentVolume);
+  const [volume, setVolume] = createSignal(50);
   const [icon, setIcon] = createSignal(volumeHighIcon);
+
+  const handleVolumeChange = (event: LocalPlayerStateUpdateEvent) => {
+    const playerState = event.state;
+    if (playerState?.volume && playerState.volume !== volume()) {
+      setVolume(playerState.volume);
+    }
+  };
+
+  onMount(() => {
+    EventStreamClient.subscribe<LocalPlayerStateUpdateEvent>(
+      LocalEventTypes.LocalPlayerStateUpdate,
+      handleVolumeChange,
+    );
+  });
+
+  onCleanup(() => {
+    EventStreamClient.unsubscribe<LocalPlayerStateUpdateEvent>(
+      LocalEventTypes.LocalPlayerStateUpdate,
+      handleVolumeChange,
+    );
+  });
 
   createEffect(() => {
     const changedVolume = volume();
