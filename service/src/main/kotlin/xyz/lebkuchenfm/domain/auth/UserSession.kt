5e19225e9ba167c5fde9525ed768d146ae80714c
Deseteral <deseteral@gmail.com>
2024-12-08 21:56:55 +0100

    Remove comment

--- service/src/main/kotlin/xyz/lebkuchenfm/domain/auth/UserSession.kt
+++ service/src/main/kotlin/xyz/lebkuchenfm/domain/auth/UserSession.kt
@@ -2,6 +2,5 @@ package xyz.lebkuchenfm.domain.auth
 
 import kotlinx.serialization.Serializable
 
-// TODO: I'm not sure if this is the right name for that class.
 @Serializable
 data class UserSession(val name: String)
