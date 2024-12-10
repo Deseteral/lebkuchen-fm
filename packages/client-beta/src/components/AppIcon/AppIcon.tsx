54019472964088512ea5aadd9c1ea96e7a2e8305
Deseteral <deseteral@gmail.com>
2024-12-10 23:28:48 +0100

    Build fix

--- packages/client-beta/src/components/AppIcon/AppIcon.tsx
+++ packages/client-beta/src/components/AppIcon/AppIcon.tsx
@@ -1,4 +1,8 @@
 import appIconsSpreadsheet from '../../icons/app-icons-spritesheet.png';
+import { createEffect, createSignal } from 'solid-js';
+
+const ICON_SPREADSHEET_ROW_COUNT = 2;
+const ICON_SPREADSHEET_COLUMN_COUNT = 4;
 
 interface AppIconProps {
   size: number;
@@ -6,21 +10,25 @@ interface AppIconProps {
 }
 
 function AppIcon(props: AppIconProps) {
-  const spriteSheetRowCount = 2;
-  const spriteSheetColumnCount = 4;
-  const backgroundPositionX = props.iconIndex[0] * props.size;
-  const backgroundPositionY = props.iconIndex[1] * props.size;
+  const [iconStyle, setIconStyle] = createSignal<string>('');
 
-  const iconStyle = `
-    width: ${props.size}px;
-    height: ${props.size}px;
-    background-image: url(${appIconsSpreadsheet});
-    background-position: ${-backgroundPositionX}px ${-backgroundPositionY}px;
-    background-size: ${spriteSheetColumnCount * props.size}px ${spriteSheetRowCount * props.size}px;
-    image-rendering: pixelated;
-  `;
+  createEffect(() => {
+    const backgroundPositionX = props.iconIndex[0] * props.size;
+    const backgroundPositionY = props.iconIndex[1] * props.size;
+    const backgroundWidth = ICON_SPREADSHEET_COLUMN_COUNT * props.size;
+    const backgroundHeight = ICON_SPREADSHEET_ROW_COUNT * props.size;
 
-  return <div style={iconStyle} />;
+    setIconStyle(`
+      width: ${props.size}px;
+      height: ${props.size}px;
+      background-image: url(${appIconsSpreadsheet});
+      background-position: ${-backgroundPositionX}px ${-backgroundPositionY}px;
+      background-size: ${backgroundWidth}px ${backgroundHeight}px;
+      image-rendering: pixelated;
+    `);
+  });
+
+  return <div style={iconStyle()} />;
 }
 
 export { AppIcon };
