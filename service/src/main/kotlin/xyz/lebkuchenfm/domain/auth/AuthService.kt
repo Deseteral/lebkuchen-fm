bed29eb7d7e974bec97b6fe153daac5006481cd7
Deseteral <deseteral@gmail.com>
2024-12-16 12:50:21 +0100

    Error responses for login flow

--- service/src/main/kotlin/xyz/lebkuchenfm/domain/auth/AuthService.kt
+++ service/src/main/kotlin/xyz/lebkuchenfm/domain/auth/AuthService.kt
@@ -1,49 +1,58 @@
 package xyz.lebkuchenfm.domain.auth
 
+import com.github.michaelbull.result.Err
+import com.github.michaelbull.result.Ok
+import com.github.michaelbull.result.Result
 import com.github.michaelbull.result.flatten
-import com.github.michaelbull.result.getOr
 import com.github.michaelbull.result.map
+import com.github.michaelbull.result.mapError
 import io.github.oshai.kotlinlogging.KotlinLogging
+import xyz.lebkuchenfm.domain.users.AddNewUserError
+import xyz.lebkuchenfm.domain.users.SetPasswordError
 import xyz.lebkuchenfm.domain.users.UsersService
 
 private val logger = KotlinLogging.logger {}
 
 class AuthService(private val usersService: UsersService) {
-    suspend fun authenticateWithCredentials(username: String, password: String): UserSession? {
+    suspend fun authenticateWithCredentials(username: String, password: String): Result<UserSession, AuthError> {
         val user = usersService.getByName(username)
 
         return when {
             user == null && usersService.getUsersCount() == 0L -> {
                 logger.info { "User '$username' is the first user to log in." }
-                // TODO: Handle errors (probably just present them to the user).
+
                 usersService.addNewUser(username)
-                    .map { usersService.setPassword(it, password) }
+                    .mapError { AuthError.CannotAddNewUserError(it) }
+                    .map { newUser ->
+                        usersService.setPassword(
+                            newUser,
+                            password,
+                        ).mapError { AuthError.CannotSetPasswordError(it) }
+                    }
                     .flatten()
-                    .getOr(null)
-                    ?.let { UserSession(it.data.name) }
+                    .map { UserSession(it.data.name) }
             }
 
             user == null -> {
                 logger.info { "User '$username' tried to log in, but does not exist." }
-                null
+                Err(AuthError.UserDoesNotExistError)
             }
 
             !user.hasPasswordSet -> {
                 logger.info { "User '${user.data.name}' is logging in for the first time." }
-                // TODO: Handle errors (probably just present them to the user).
                 usersService.setPassword(user, password)
                     .map { UserSession(it.data.name) }
-                    .getOr(null)
+                    .mapError { AuthError.CannotSetPasswordError(it) }
             }
 
             else -> {
                 if (usersService.checkPassword(user, password)) {
                     logger.info { "User '${user.data.name}' logged in." }
                     usersService.updateLastLoginDate(user)
-                    UserSession(user.data.name)
+                    Ok(UserSession(user.data.name))
                 } else {
                     logger.info { "User '${user.data.name}' tried to log in, but provided wrong password." }
-                    null
+                    Err(AuthError.BadCredentialsError)
                 }
             }
         }
@@ -52,4 +61,11 @@ class AuthService(private val usersService: UsersService) {
     suspend fun authenticateWithApiToken(token: String): UserSession? {
         return usersService.getByApiToken(token)?.let { UserSession(it.data.name) }
     }
+}
+
+sealed class AuthError {
+    data class CannotSetPasswordError(val setPasswordError: SetPasswordError) : AuthError()
+    data class CannotAddNewUserError(val addNewUserError: AddNewUserError) : AuthError()
+    data object UserDoesNotExistError : AuthError()
+    data object BadCredentialsError : AuthError()
 }
