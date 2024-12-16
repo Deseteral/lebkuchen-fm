355b9ce6fd2a9427359b53e3f46edbeb14af598f
Deseteral <deseteral@gmail.com>
2024-12-16 15:38:55 +0100

    Type for YouTubeId

--- service/src/main/kotlin/xyz/lebkuchenfm/domain/songs/SongsRepository.kt
+++ service/src/main/kotlin/xyz/lebkuchenfm/domain/songs/SongsRepository.kt
@@ -1,13 +1,14 @@
 package xyz.lebkuchenfm.domain.songs
 
 import com.github.michaelbull.result.Result
+import xyz.lebkuchenfm.domain.youtube.YoutubeVideoId
 
 interface SongsRepository {
     suspend fun insert(song: Song): Result<Song, InsertSongError>
     suspend fun findAllOrderByNameAsc(): List<Song>
     suspend fun findByName(name: String): Song?
-    suspend fun findByYoutubeId(youtubeId: String): Song?
-    suspend fun findByYoutubeIds(youtubeIds: List<String>): List<Song>
+    suspend fun findByYoutubeId(youtubeId: YoutubeVideoId): Song?
+    suspend fun findByYoutubeIds(youtubeIds: List<YoutubeVideoId>): List<Song>
     suspend fun findRandom(limit: Int, phrase: String): List<Song>
     suspend fun incrementPlayCountByName(name: String): Song?
 }
--- service/src/main/kotlin/xyz/lebkuchenfm/external/youtube/YouTubeDataRepository.kt
+++ service/src/main/kotlin/xyz/lebkuchenfm/external/youtube/YouTubeDataRepository.kt
@@ -3,19 +3,20 @@ package xyz.lebkuchenfm.external.youtube
 import com.github.michaelbull.result.get
 import xyz.lebkuchenfm.domain.youtube.YouTubeRepository
 import xyz.lebkuchenfm.domain.youtube.YoutubeVideo
+import xyz.lebkuchenfm.domain.youtube.YoutubeVideoId
 
 class YouTubeDataRepository(private val youtubeClient: YoutubeClient) : YouTubeRepository {
-    override suspend fun findVideoById(youtubeId: String): YoutubeVideo? {
-        return youtubeClient.getVideoName(youtubeId).get()?.let { YoutubeVideo(youtubeId, it) }
+    override suspend fun findVideoById(youtubeId: YoutubeVideoId): YoutubeVideo? {
+        return youtubeClient.getVideoName(youtubeId.value).get()?.let { YoutubeVideo(youtubeId, it) }
     }
 
     override suspend fun findVideoByPhrase(phrase: String): YoutubeVideo? {
-        return youtubeClient.findVideo(phrase).get()?.let { YoutubeVideo(it.id, it.snippet.title) }
+        return youtubeClient.findVideo(phrase).get()?.let { YoutubeVideo(YoutubeVideoId(it.id), it.snippet.title) }
     }
 
     override suspend fun findVideosByPlaylistId(playlistId: String): List<YoutubeVideo> {
         return youtubeClient.getPlaylistVideos(playlistId).get()
-            ?.map { YoutubeVideo(it.id, it.snippet.title) }
+            ?.map { YoutubeVideo(YoutubeVideoId(it.id), it.snippet.title) }
             ?: emptyList()
     }
 }
