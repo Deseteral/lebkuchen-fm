147f91198201ffd68c127de743da29052a03a8cc
Deseteral <deseteral@gmail.com>
2025-01-09 12:20:11 +0100

    bugfix: Save YouTube ID instead of name in history

--- service/src/main/kotlin/xyz/lebkuchenfm/domain/history/HistoryEntry.kt
+++ service/src/main/kotlin/xyz/lebkuchenfm/domain/history/HistoryEntry.kt
@@ -1,10 +1,11 @@
 package xyz.lebkuchenfm.domain.history
 
 import kotlinx.datetime.Instant
+import xyz.lebkuchenfm.domain.youtube.YoutubeVideoId
 
 data class HistoryEntry(
     val date: Instant,
-    val youtubeId: String,
+    val youtubeId: YoutubeVideoId,
     // TODO: This should be a User object instead.
     val user: String?,
 )
