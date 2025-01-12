90eff2c15eb6fdd97b5473ca4ffcba2e6e25aae3
Deseteral <deseteral@gmail.com>
2025-01-12 20:55:43 +0100

    Formatting

--- packages/client-beta/src/apps/Soundboard/soundboard-service.ts
+++ packages/client-beta/src/apps/Soundboard/soundboard-service.ts
@@ -1,7 +1,8 @@
 abstract class SoundboardService {
   static async playXSound(soundName: string) {
-    return fetch(`/api/soundboard/play?soundName=${encodeURIComponent(soundName)}`, { method: 'POST' })
-      .catch((err) => console.error(err));
+    return fetch(`/api/soundboard/play?soundName=${encodeURIComponent(soundName)}`, {
+      method: 'POST',
+    }).catch((err) => console.error(err));
   }
 }
 
