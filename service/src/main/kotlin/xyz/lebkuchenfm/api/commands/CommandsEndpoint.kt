a0ce960c8adc4d9f356d437d5404a45cc6f39e0c
Deseteral <deseteral@gmail.com>
2025-01-12 17:03:07 +0100

    Command endpoint checks content-type for correct behaviour

--- service/src/main/kotlin/xyz/lebkuchenfm/api/commands/CommandsEndpoint.kt
+++ service/src/main/kotlin/xyz/lebkuchenfm/api/commands/CommandsEndpoint.kt
@@ -1,26 +1,39 @@
 package xyz.lebkuchenfm.api.commands
 
 import io.github.oshai.kotlinlogging.KotlinLogging
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.post
 import kotlinx.serialization.Serializable
 import xyz.lebkuchenfm.api.getUserSession
+import xyz.lebkuchenfm.api.respondWithProblem
 import xyz.lebkuchenfm.domain.commands.CommandExecutorService
 import xyz.lebkuchenfm.domain.commands.ExecutionContext
 
 private val logger = KotlinLogging.logger {}
 
 fun Route.commandsRouting(commandExecutorService: CommandExecutorService) {
-    post("/commands/text") {
+    post("/commands/execute") {
         val session = call.getUserSession()
-
-        val text = call.receive<TextCommandRequest>().text
-        logger.info { "Received $text command from ${session.name}" }
-
         val context = ExecutionContext(session)
-        val processingResult = commandExecutorService.executeFromText(text, context)
+
+        val processingResult = when (call.request.headers["Content-Type"]) {
+            "plain/text" -> {
+                val text = call.receive<TextCommandRequest>().text
+                logger.info { "Received $text command from ${session.name}" }
+                commandExecutorService.executeFromText(text, context)
+            }
+            else -> {
+                call.respondWithProblem(
+                    title = "Invalid content type.",
+                    detail = "Unsupported content type.",
+                    status = HttpStatusCode.UnsupportedMediaType,
+                )
+                return@post
+            }
+        }
 
         val response = TextCommandResponse(textResponse = processingResult.message.markdown)
         call.respond(response)
