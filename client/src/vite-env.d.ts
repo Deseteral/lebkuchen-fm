673a51439e43f1bfb59a1bfa8e7871659acd34e4
Deseteral <deseteral@gmail.com>
2025-02-14 18:07:11 +0100

    Formatting fixes

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,2 +1,2 @@
 /// <reference types="vite/client" />
-declare const __APP_VERSION__: string
+declare const __APP_VERSION__: string;
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -32,6 +32,6 @@ export default defineConfig({
     },
   },
   define: {
-    '__APP_VERSION__': JSON.stringify(process.env.npm_package_version),
-  }
+    __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+  },
 });
