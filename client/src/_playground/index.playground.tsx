56608bddbafd24cfdbc76d57c05ab4f0cae4e6e0
Kacper Stodolak <kapi199726@gmail.com>
2025-03-19 20:35:25 +0100

    Playground - arrow functions to regular functions

--- client/src/_playground/index.playground.tsx
+++ client/src/_playground/index.playground.tsx
@@ -3,13 +3,15 @@ import '../styles.css';
 import styles from './styles.playground.module.css';
 import { ButtonsPlayground } from './Buttons.playground';
 
-const Showcase = () => (
-  <div class={styles.componentsPlayground}>
-    <h1>Components playground</h1>
-    <section class={styles.playgroundSection}>
-      <ButtonsPlayground />
-    </section>
-  </div>
-);
+function Showcase() {
+  return (
+    <div class={styles.componentsPlayground}>
+      <h1>Components playground</h1>
+      <section class={styles.playgroundSection}>
+        <ButtonsPlayground />
+      </section>
+    </div>
+  );
+}
 
 render(() => <Showcase />, document.getElementById('root')!);
