5b13472a28d8c9204c8b1589e6d95aa5ccbd81f9
Kacper Stodolak <kapi199726@gmail.com>
2025-03-25 21:15:48 +0100

    Dev vite config changes reverted

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -18,23 +18,12 @@ export default defineConfig({
       '@components': path.resolve(__dirname, 'src/components'),
     },
   },
-  // server: {
-  //   port: 9090,
-  //   proxy: {
-  //     '/api/event-stream': {
-  //       target: 'ws://localhost:8080',
-  //       ws: true,
-  //       changeOrigin: true,
-  //     },
-  //     '/api': 'http://localhost:8080',
-  //   },
-  // },
   server: {
     port: 9090,
     proxy: {
-      '/api': 'https://lebkuchen-fm.fly.dev',
+      '/api': 'http://localhost:8080',
       '/api/event-stream': {
-        target: 'wss://lebkuchen-fm.fly.dev',
+        target: 'ws://localhost:8080',
         ws: true,
         changeOrigin: true,
       },
