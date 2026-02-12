48b65fb2851d7368236bef32179387f6e360efa7
Deseteral <deseteral@gmail.com>
2026-02-13 00:58:46 +0100

    Clean up

--- client/src/apps/Terminal/Terminal.tsx
+++ client/src/apps/Terminal/Terminal.tsx
@@ -44,51 +44,43 @@ function Buffer() {
   let bufferElement!: HTMLDivElement;
   let promptElement!: HTMLInputElement;
 
-  const scrollToBottom = () => (bufferElement.scrollTop = bufferElement.scrollHeight);
-
-  const setPrompt = (value: string) => (promptElement.value = value);
-
   const [lines, setLines] = createSignal<string[]>([WELCOME_MESSAGE, '']);
-  const appendLines = (newLines: string[]) => {
+
+  const scrollToBottom = (): void => {
+    bufferElement.scrollTop = bufferElement.scrollHeight;
+  };
+
+  const setPrompt = (value: string): void => {
+    promptElement.value = value;
+  };
+
+  const appendBufferLines = (newLines: string[]): void => {
     setLines([...lines(), ...newLines]);
     scrollToBottom();
   };
 
-  // TODO: Prompt history could be saved to localStorage.
-  const [history, setHistory] = createSignal<string[]>([]);
-  const [historyIdx, setHistoryIdx] = createSignal<number>(-1);
-  const appendHistory = (prompt: string) => setHistory([prompt, ...history()]);
-  const fromHistory = (offset: 1 | -1): string => {
-    const nextIdx = clamp(historyIdx() + offset, -1, history().length - 1);
-    setHistoryIdx(nextIdx);
-    return nextIdx === -1 ? '' : history()[nextIdx]!;
-  };
-
-  const evalPrompt = async (prompt: string) => {
-    appendLines([`> ${prompt}`]);
-
-    if (!!prompt) appendHistory(prompt);
-    setHistoryIdx(-1);
+  const evalPrompt = async (prompt: string): Promise<void> => {
+    appendBufferLines([`> ${prompt}`]);
 
     const [command, ...args] = prompt.split(' ').filter((s) => !!s);
     if (!command) {
-      appendLines(['']);
+      appendBufferLines(['']);
       return;
     }
 
     const program = programs[command];
     if (!program) {
-      appendLines([`${command}: command not found`]);
+      appendBufferLines([`${command}: command not found`]);
       return;
     }
 
     try {
       const output = await program(args);
-      appendLines(output);
+      appendBufferLines(output);
     } catch (err) {
-      appendLines(['Something went wrong.']);
+      appendBufferLines(['Something went wrong.']);
       if (err instanceof Error) {
-        appendLines(err.message.split('\n'));
+        appendBufferLines(err.message.split('\n'));
       }
     }
   };
@@ -109,22 +101,10 @@ function Buffer() {
                 evalPrompt(promptElement.value.trim());
                 setPrompt('');
                 break;
-
-              case 'ArrowUp':
-                setPrompt(fromHistory(+1));
-                break;
-
-              case 'ArrowDown':
-                setPrompt(fromHistory(-1));
-                break;
             }
           }}
         />
       </div>
     </div>
   );
-}
-
-function clamp(value: number, min: number, max: number): number {
-  return Math.min(Math.max(value, min), max);
 }
--- client/src/apps/Terminal/programs.ts
+++ client/src/apps/Terminal/programs.ts
@@ -1,15 +1,11 @@
 import { executeCommand } from '../../services/command-service';
 
-async function fm(args: string[]) {
+async function fm(args: string[]): Promise<string[]> {
   const response = await executeCommand(`/fm ${args.join(' ')}`);
   return response.split('\n');
 }
 
-async function echo(args: string[]) {
-  return args;
-}
-
-async function help() {
+async function help(): Promise<string[]> {
   const listOfCommands = Object.keys(programs).sort((a, b) => a.localeCompare(b));
   return [
     'Available commands:',
@@ -18,7 +14,6 @@ async function help() {
 }
 
 export const programs: { [key: string]: (args: string[]) => Promise<string[]> } = {
-  echo,
   fm,
-  help
+  help,
 };
