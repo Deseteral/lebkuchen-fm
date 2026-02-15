216780264ad6e023033e3da3a8ee641c79c42237
Kacper Stodolak <kapi199726@gmail.com>
2026-02-15 15:55:38 +0100

    Player: fixed raced condition errors due to websocket connection readiness

--- client/src/apps/Player/components/YouTubePlayer/YouTubePlayer.tsx
+++ client/src/apps/Player/components/YouTubePlayer/YouTubePlayer.tsx
@@ -1,15 +1,46 @@
-import { onMount, onCleanup } from 'solid-js';
+import { onCleanup, onMount } from 'solid-js';
 import { YoutubePlayerService } from '../../services/youtube-player-service';
+import { EventStreamClient } from '../../../../services/event-stream-client';
+import {
+  LocalEventTypes,
+  LocalWebsocketConnectionReadyEvent,
+} from '../../../../types/local-events';
+import { SocketConnectionClient } from '../../../../services/socket-connection-client';
 
 const YOUTUBE_PLAYER_DOM_ID = 'youtube-player';
 
 function YouTubePlayer() {
+  const initializeYoutubePlayer = () => {
+    if (SocketConnectionClient.ready() && !YoutubePlayerService.initialized()) {
+      console.log('[YouTubePlayer] Service initializing');
+      YoutubePlayerService.initialize(YOUTUBE_PLAYER_DOM_ID);
+    }
+  };
+
+  const onWebsocketConnectionReady = () => {
+    console.log('[YouTubePlayer] Websocket connection ready event');
+    initializeYoutubePlayer();
+  };
+
+  const unsubscribeFromWebsocketConnectionReadyEvent = () => {
+    EventStreamClient.unsubscribe<LocalWebsocketConnectionReadyEvent>(
+      LocalEventTypes.LocalWebsocketConnectionReady,
+      onWebsocketConnectionReady,
+    );
+  };
+
+  EventStreamClient.subscribe<LocalWebsocketConnectionReadyEvent>(
+    LocalEventTypes.LocalWebsocketConnectionReady,
+    onWebsocketConnectionReady,
+  );
+
   onMount(() => {
-    YoutubePlayerService.initialize(YOUTUBE_PLAYER_DOM_ID);
+    initializeYoutubePlayer();
   });
 
   onCleanup(() => {
     YoutubePlayerService.cleanup();
+    unsubscribeFromWebsocketConnectionReadyEvent();
   });
 
   return <div id={YOUTUBE_PLAYER_DOM_ID} />;
