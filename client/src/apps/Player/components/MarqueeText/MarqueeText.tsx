e9a1db8d9ff5d79ccd63561a766c58b424b4959a
Kacper Stodolak <kapi199726@gmail.com>
2026-02-24 19:37:14 +0100

    Marquee effect on currently playing text

--- client/src/apps/Player/components/MarqueeText/MarqueeText.tsx
+++ client/src/apps/Player/components/MarqueeText/MarqueeText.tsx
@@ -1,5 +1,6 @@
 import { createEffect, createSignal, on, onCleanup } from 'solid-js';
 import styles from './MarqueeText.module.css';
+import clsx from 'clsx';
 
 const SCROLL_SPEED_PX_PER_SEC = 50;
 const SEPARATOR = ' \u2022\u2022\u2022 ';
@@ -11,14 +12,17 @@ interface MarqueeTextProps {
 function MarqueeText(props: MarqueeTextProps) {
   const [shouldScroll, setShouldScroll] = createSignal(false);
   const [duration, setDuration] = createSignal(0);
-  let containerRef!: HTMLDivElement;
-  let measureRef!: HTMLSpanElement;
+  let containerRef: HTMLDivElement | null = null;
+  let measureRef: HTMLSpanElement | null = null;
 
   const measure = () => {
-    if (!containerRef || !measureRef) return;
+    if (!containerRef || !measureRef) {
+      return;
+    }
+
     requestAnimationFrame(() => {
-      const textWidth = measureRef.scrollWidth;
-      const containerWidth = containerRef.clientWidth;
+      const textWidth = measureRef!.scrollWidth;
+      const containerWidth = containerRef!.clientWidth;
       const overflows = textWidth > containerWidth;
 
       setShouldScroll(overflows);
@@ -29,9 +33,9 @@ function MarqueeText(props: MarqueeTextProps) {
     });
   };
 
-  createEffect(on(() => props.text, measure));
-
   const resizeObserver = new ResizeObserver(measure);
+
+  createEffect(on(() => props.text, measure));
 
   createEffect(() => {
     if (containerRef) {
@@ -49,7 +53,7 @@ function MarqueeText(props: MarqueeTextProps) {
         {props.text}
       </span>
       <span
-        class={shouldScroll() ? styles.scrollingText : undefined}
+        class={clsx(shouldScroll() && styles.scrollingText)}
         style={shouldScroll() ? { '--marquee-duration': `${duration()}s` } : undefined}
       >
         {shouldScroll() ? `${props.text}${SEPARATOR}${props.text}${SEPARATOR}` : props.text}
