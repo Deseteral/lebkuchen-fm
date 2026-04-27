046d7f1fdea9183a9af48b44506a9c5bbfb6d765
Michal Gorzalczany <michal@gorzalczany.pl>
2026-04-27 16:53:38 +0200

    propose roles and scopes

--- service/src/main/kotlin/xyz/lebkuchenfm/domain/auth/Scope.kt
+++ service/src/main/kotlin/xyz/lebkuchenfm/domain/auth/Scope.kt
@@ -5,9 +5,13 @@ package xyz.lebkuchenfm.domain.auth
  * Values follow the OAuth2 scope naming convention: "resource:action".
  */
 enum class Scope(val value: String) {
+    PLAYER_CONTROL("player:control"),
     PLAYER_QUEUE("player:queue"),
+    PLAYER_SKIP("player:skip"),
     XSOUNDS_PLAY("xsounds:play"),
     XSOUNDS_UPLOAD("xsounds:upload"),
+    XSOUNDS_MANAGE("xsounds:manage"),
+    USERS_MANAGE("users:manage"),
     ;
 
     companion object {
