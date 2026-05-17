4572cc01abf8b1ec53d9a90f911e4a7ae3189889
Michal Gorzalczany <michal@gorzalczany.pl>
2026-05-17 19:25:06 +0200

    reformat

--- client/src/apps/ApplicationHost.tsx
+++ client/src/apps/ApplicationHost.tsx
@@ -4,20 +4,26 @@ import type { ApplicationId } from './application-definitions';
 import type { IntegrationsResponse } from '../services/integrations-service';
 
 const Player = lazy(async () => ({ default: (await import('./Player/Player')).Player }));
-const Soundboard = lazy(async () => ({ default: (await import('./Soundboard/Soundboard')).Soundboard }));
+const Soundboard = lazy(async () => ({
+  default: (await import('./Soundboard/Soundboard')).Soundboard,
+}));
 const Terminal = lazy(async () => ({ default: (await import('./Terminal/Terminal')).Terminal }));
-const SoundUpload = lazy(async () => ({ default: (await import('./SoundUpload/SoundUpload')).SoundUpload }));
+const SoundUpload = lazy(async () => ({
+  default: (await import('./SoundUpload/SoundUpload')).SoundUpload,
+}));
 const Users = lazy(async () => ({ default: (await import('./Users/Users')).Users }));
-const AppLauncher = lazy(async () => ({ default: (await import('./AppLauncher/AppLauncher')).AppLauncher }));
-const ControlPanelApp = lazy(
-  async () => ({ default: (await import('./ControlPanel/ControlPanelApp')).ControlPanelApp }),
-);
-const SoundSettingsApp = lazy(
-  async () => ({ default: (await import('./ControlPanel/SoundSettingsApp')).SoundSettingsApp }),
-);
-const IntegrationSettingsApp = lazy(
-  async () => ({ default: (await import('./ControlPanel/IntegrationSettingsApp')).IntegrationSettingsApp }),
-);
+const AppLauncher = lazy(async () => ({
+  default: (await import('./AppLauncher/AppLauncher')).AppLauncher,
+}));
+const ControlPanelApp = lazy(async () => ({
+  default: (await import('./ControlPanel/ControlPanelApp')).ControlPanelApp,
+}));
+const SoundSettingsApp = lazy(async () => ({
+  default: (await import('./ControlPanel/SoundSettingsApp')).SoundSettingsApp,
+}));
+const IntegrationSettingsApp = lazy(async () => ({
+  default: (await import('./ControlPanel/IntegrationSettingsApp')).IntegrationSettingsApp,
+}));
 
 interface AppMountProps {
   id: ApplicationId;
@@ -39,9 +45,11 @@ function IntegrationSettingsMount() {
     <Show when={ApplicationServer.isOpen('integration-settings')}>
       <Suspense>
         <IntegrationSettingsApp
-          data={ApplicationServer.getInstance('integration-settings')?.payload as
-            | IntegrationsResponse
-            | undefined}
+          data={
+            ApplicationServer.getInstance('integration-settings')?.payload as
+              | IntegrationsResponse
+              | undefined
+          }
         />
       </Suspense>
     </Show>
